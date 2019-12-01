58f91f0b8cd5ea5ada49c8f8c55e750230c83a08
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-02 00:15:26 +0100

    Catch error in html

--- wasm/html/index.js
+++ wasm/html/index.js
@@ -4,11 +4,23 @@ document.getElementById("run_button").addEventListener("click", function() {
    const day_element = document.getElementById('day');
    const part_element = document.getElementById('part');
    const input_element = document.getElementById('input');
+   const output_element = document.getElementById('output');
 
    const day = parseInt(day_element.options[day_element.selectedIndex].value);
    const part = parseInt(part_element.options[part_element.selectedIndex].value);
    const input = input_element.value;
 
-   const solution = wasm.solve(day, part, input);
-   document.getElementById('output').textContent = solution;
+   input_element.addEventListener('input', function() {
+      output_element.textContent = '';
+   }, false);
+
+
+   let message;
+   try {
+      message = wasm.solve(day, part, input);
+   } catch (e) {
+      console.log(e);
+      message = 'ERROR: Invalid input or bug in solution';
+   }
+   output_element.textContent = message;
 });
