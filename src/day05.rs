26ca38ed2a2091b5172e3e662a030165003b4786
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-05 07:43:52 +0100

    Day 5, part 1

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,84 @@
-pub fn part1(_input_string: &str) -> String {
-    String::from("")
+struct Program {
+    memory: Vec<i64>,
+    instruction_pointer: usize,
+    last_output: i64,
+}
+
+impl Program {
+    fn parse(input: &str) -> Program {
+        Program {
+            memory: input
+                .split(',')
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect(),
+            instruction_pointer: 0,
+            last_output: 0,
+        }
+    }
+
+    fn run(&mut self) -> i64 {
+        loop {
+            if !self.evaluate() {
+                break self.memory[0];
+            }
+        }
+    }
+
+    fn is_immediate(opcode_and_parameter_modes: i64, position: u32) -> bool {
+        let divider = 10_i64.pow(position + 1);
+        (opcode_and_parameter_modes / divider) % 10 == 1
+    }
+
+    fn parameter_value(&self, opcode_and_parameter_modes: i64, parameter_position: u32) -> i64 {
+        let parameter = self.memory[self.instruction_pointer + parameter_position as usize];
+        if Program::is_immediate(opcode_and_parameter_modes, parameter_position) {
+            parameter
+        } else {
+            self.memory[parameter as usize]
+        }
+    }
+
+    fn evaluate(&mut self) -> bool {
+        let opcode_and_parameter_modes = self.memory[self.instruction_pointer];
+        let opcode = opcode_and_parameter_modes % 100;
+        match opcode {
+            99 => false,
+            1 | 2 => {
+                let parameter1 = self.parameter_value(opcode_and_parameter_modes, 1);
+                let parameter2 = self.parameter_value(opcode_and_parameter_modes, 2);
+                let output_location = self.memory[self.instruction_pointer + 3] as usize;
+                self.memory[output_location] = if opcode == 1 {
+                    parameter1 + parameter2
+                } else {
+                    parameter1 * parameter2
+                };
+                self.instruction_pointer += 4;
+                true
+            }
+            3 => {
+                // Takes a single integer as input and saves it to the address given by its only parameter.
+                let save_address = self.memory[self.instruction_pointer + 1];
+                self.memory[save_address as usize] = 1; // Input to be specified.
+                self.instruction_pointer += 2;
+                true
+            }
+            4 => {
+                // Takes a single integer as input and saves it to the address given by its only parameter.
+                self.last_output = self.parameter_value(opcode_and_parameter_modes, 1);
+                self.instruction_pointer += 2;
+                true
+            }
+            _ => {
+                panic!("Invalid opcode: {}", opcode);
+            }
+        }
+    }
+}
+
+pub fn part1(input_string: &str) -> String {
+    let mut program = Program::parse(input_string);
+    program.run();
+    program.last_output.to_string()
 }
 
 pub fn part2(_input_string: &str) -> String {
@@ -8,9 +87,7 @@ pub fn part2(_input_string: &str) -> String {
 
 #[test]
 pub fn tests_part1() {
-    assert_eq!(part1(""), "");
-
-    // assert_eq!(part1(include_str!("day05_input.txt")), "");
+    assert_eq!(part1(include_str!("day05_input.txt")), "15097178");
 }
 
 #[test]
