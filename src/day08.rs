deabba878f05ea7798cffc5f599ebebf36078b08
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-08 18:29:26 +0100

    Day 8, part 2

--- src/day08.rs
+++ src/day08.rs
@@ -1,32 +1,65 @@
+use std::str;
 extern crate bytecount;
 
-fn count(slice: &[u8], wanted_by: u8) -> usize {
-    bytecount::count(slice, wanted_by)
-}
+const PIXELS_WIDE: u32 = 25;
+const PIXELS_TALL: u32 = 6;
+const LAYER_SIZE: usize = (PIXELS_WIDE * PIXELS_TALL) as usize;
 
 pub fn part1(input_string: &str) -> String {
-    let layer_size = 150;
-    assert_eq!(input_string.len() % 150, 0);
+    assert_eq!(input_string.len() % LAYER_SIZE, 0);
 
     let (_, slice) = input_string
         .as_bytes()
-        .chunks(layer_size)
+        .chunks(LAYER_SIZE)
         .map(|slice| {
-            let num_zeros = count(slice, b'0');
+            let num_zeros = bytecount::count(slice, b'0');
             (num_zeros, slice)
         })
         .min_by_key(|(num_zeros, _)| *num_zeros)
         .expect("Nothing found by min_by_key()");
 
-    let count_1 = count(slice, b'1');
-    let count_2 = count(slice, b'2');
+    let count_1 = bytecount::count(slice, b'1');
+    let count_2 = bytecount::count(slice, b'2');
     let result = count_1 * count_2;
 
     result.to_string()
 }
 
-pub fn part2(_input_string: &str) -> String {
-    String::from("")
+pub fn part2(input_string: &str) -> String {
+    part2_sized(input_string, PIXELS_WIDE, PIXELS_TALL)
+}
+
+pub fn part2_sized(input_string: &str, width: u32, height: u32) -> String {
+    let layer_size = (width * height) as usize;
+    let mut image = vec![b'2'; layer_size];
+
+    input_string
+        .as_bytes()
+        .chunks(layer_size)
+        .for_each(|layer| {
+            image
+                .iter_mut()
+                .zip(layer.iter())
+                .for_each(|(image_pixel, &layer_pixel)| {
+                    if *image_pixel == b'2' {
+                        *image_pixel = layer_pixel;
+                    }
+                });
+        });
+
+    let image = image
+        .chunks(width as usize)
+        .fold(Vec::<u8>::new(), |mut acc, layer| {
+            acc.extend_from_slice(layer);
+            acc.push(b'\n');
+            acc
+        });
+
+    str::from_utf8(&image)
+        .unwrap()
+        .replace('1', "█")
+        .replace('0', " ")
+        .to_string()
 }
 
 #[test]
@@ -36,7 +69,8 @@ pub fn tests_part1() {
 
 #[test]
 fn tests_part2() {
-    assert_eq!(part2(""), "");
+    assert_eq!(part2_sized("0222112222120000", 2, 2), " █\n█ \n");
 
-    // assert_eq!(part2(include_str!("day08_input.txt")), "");
+    assert_eq!(part2(include_str!("day08_input.txt")),
+"███   ██  ███  ████ ███  \n█  █ █  █ █  █    █ █  █ \n███  █    █  █   █  ███  \n█  █ █    ███   █   █  █ \n█  █ █  █ █    █    █  █ \n███   ██  █    ████ ███  \n");
 }
