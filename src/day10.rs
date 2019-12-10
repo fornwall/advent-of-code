e415321d03173adfe2e750d1fc35dee89c324bb3
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-10 23:44:10 +0100

    More iterators

--- src/day10.rs
+++ src/day10.rs
@@ -26,28 +26,26 @@ pub fn parse_points(input_string: &str) -> Vec<(usize, usize)> {
 
 /// Return (max_seen, (x, y)) of station.
 pub fn determine_station(points: &[(usize, usize)]) -> (usize, (usize, usize)) {
-    let mut max_seen = 0 as usize;
-    let mut base_location = (0, 0);
-
-    for (i, &this_point) in points.iter().enumerate() {
-        let mut seen = HashSet::new();
-        for (j, other_point) in points.iter().enumerate() {
-            if i != j {
-                let mut distance_x = other_point.0 as i64 - this_point.0 as i64;
-                let mut distance_y = other_point.1 as i64 - this_point.1 as i64;
-                let divisor = gcd(distance_x.abs(), distance_y.abs());
-                distance_x /= divisor;
-                distance_y /= divisor;
-                seen.insert((distance_x, distance_y));
-            }
-        }
-        if seen.len() > max_seen {
-            max_seen = seen.len();
-            base_location = this_point;
-        }
-    }
-
-    (max_seen, base_location)
+    points
+        .iter()
+        .map(|&this_point| {
+            let seen_count = points
+                .iter()
+                .filter(|&&point| point != this_point)
+                .fold(HashSet::new(), |mut seen, other_point| {
+                    let mut distance_x = other_point.0 as i64 - this_point.0 as i64;
+                    let mut distance_y = other_point.1 as i64 - this_point.1 as i64;
+                    let divisor = gcd(distance_x.abs(), distance_y.abs());
+                    distance_x /= divisor;
+                    distance_y /= divisor;
+                    seen.insert((distance_x, distance_y));
+                    seen
+                })
+                .len();
+            (seen_count, this_point)
+        })
+        .max_by_key(|&(seen_count, _)| seen_count)
+        .unwrap()
 }
 
 pub fn part1(input_string: &str) -> String {
