538ec25ae78224c9e4ac30beca8ed662a5997631
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-21 23:54:02 +0100

    aa

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,43 @@
 use crate::int_code::Program;
 
+fn run(intcode_program_string: &str, ascii_program_string: &str) -> String {
+    let mut intcode_program = Program::parse(intcode_program_string);
+    intcode_program.run();
+    intcode_program.output_values.clear();
+
+    let mut ascii_program = String::new();
+    // Jump if hole is in A, B or C...
+    ascii_program.push_str("NOT A J\n");
+    ascii_program.push_str("NOT B T\nOR T J\n");
+    ascii_program.push_str("NOT C T\nOR T J\n");
+    // ... and there is ground
+    ascii_program.push_str("AND D J\n");
+
+    ascii_program.push_str("AND E T\nOR H T\nAND T J\n");
+
+    ascii_program.push_str("RUN\n");
+
+    intcode_program.input_string(ascii_program.as_str());
+    intcode_program.run();
+
+    if let Some(value) = intcode_program
+        .output_values
+        .iter()
+        .find(|&&value| value > 255)
+    {
+        return value.to_string();
+    } else {
+        let u8_array: Vec<u8> = intcode_program
+            .output_values
+            .iter()
+            .map(|&value| value as u8)
+            .collect();
+        println!("{}", std::str::from_utf8(&u8_array).unwrap());
+    }
+    String::from("")
+
+}
+
 // Three instructions: AND, OR and NOT.
 // Six possible first registers: A, B, C, D, T and J.
 // Two possible second registers: T, J.
@@ -33,10 +71,15 @@ pub fn part2(input_string: &str) -> String {
     intcode_program.output_values.clear();
 
     let mut ascii_program = String::new();
-    ascii_program.push_str("NOT A T\nOR T J\n");
+    // Jump if hole is in A, B or C...
+    ascii_program.push_str("NOT A J\n");
     ascii_program.push_str("NOT B T\nOR T J\n");
     ascii_program.push_str("NOT C T\nOR T J\n");
+    // ... and there is ground
     ascii_program.push_str("AND D J\n");
+
+    ascii_program.push_str("AND E T\nOR H T\nAND T J\n");
+
     ascii_program.push_str("RUN\n");
 
     intcode_program.input_string(ascii_program.as_str());
@@ -66,5 +109,5 @@ pub fn tests_part1() {
 
 #[test]
 fn tests_part2() {
-    assert_eq!(part2(include_str!("day21_input.txt")), "");
+    assert_eq!(part2(include_str!("day21_input.txt")), "1141236756");
 }
