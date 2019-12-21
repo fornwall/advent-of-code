f18be8e523ec628007fa686d9623e9b74482cd7e
Fredrik Fornwall <fredrik@fornwall.net>
2019-12-21 22:34:32 +0100

    Day 21, part 1

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,30 @@
-pub fn part1(_input_string: &str) -> String {
-    String::from("")
+use crate::int_code::Program;
+
+// Three instructions: AND, OR and NOT.
+// Six possible first registers: A, B, C, D, T and J.
+// Two possible second registers: T, J.
+// Possibilities: 3*6*2 = 36.
+pub fn part1(input_string: &str) -> String {
+    let mut intcode_program = Program::parse(input_string);
+    intcode_program.run();
+    intcode_program.output_values.clear();
+
+    let mut ascii_program = String::new();
+    ascii_program.push_str("NOT A T\nOR T J\n");
+    ascii_program.push_str("NOT B T\nOR T J\n");
+    ascii_program.push_str("NOT C T\nOR T J\n");
+    ascii_program.push_str("AND D J\n");
+    ascii_program.push_str("WALK\n");
+
+    intcode_program.input_string(ascii_program.as_str());
+    intcode_program.run();
+
+    intcode_program
+        .output_values
+        .iter()
+        .find(|&&value| value > 255)
+        .unwrap()
+        .to_string()
 }
 
 pub fn part2(_input_string: &str) -> String {
@@ -8,9 +33,7 @@ pub fn part2(_input_string: &str) -> String {
 
 #[test]
 pub fn tests_part1() {
-    assert_eq!(part1(""), "");
-
-    // assert_eq!(part1(include_str!("day21_input.txt")), "");
+    assert_eq!(part1(include_str!("day21_input.txt")), "19358688");
 }
 
 #[test]
