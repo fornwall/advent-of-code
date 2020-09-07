77d313db925b4b48b1ff0e32f79acebb7da8fb0b
Fredrik Fornwall <fredrik@fornwall.net>
2020-09-07 12:25:33 +0200

    Update netlify function

--- crates/wasm/functions/solve.js
+++ crates/wasm/functions/solve.js
@@ -14,12 +14,24 @@ exports.handler = function(event, context, callback) {
   const parameters = event.queryStringParameters;
   const day = parseInt(parameters.day || '1');
   const part = parseInt(parameters.part || '1')
-  const input = event.body;
 
+  if (!(day >= 1 && day <= 25)) {
+    return callback(null, {
+      statusCode: 400,
+      body: 'Invalid day - must be integer between 1 and 25'
+    });
+  } else if (!(part >= 1 && part <= 2)) {
+    return callback(null, {
+      statusCode: 400,
+      body: 'Invalid part - must be 1 or 2'
+    });
+  }
+
+  const input = event.body;
   const solution = solve(day, part, input);
 
   return callback(null, {
     statusCode: 200,
-    body: "day=" + day + ", part=" + part + ", input=" + input + ", Solution: " + solution
+    body: solution
   });
 }
