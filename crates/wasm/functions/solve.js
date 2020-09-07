862d1a2b2442ee570bf005abc2a10ed969758444
Fredrik Fornwall <fredrik@fornwall.net>
2020-09-07 12:13:41 +0200

    Evolve netlify function

--- crates/wasm/functions/solve.js
+++ crates/wasm/functions/solve.js
@@ -1,18 +1,25 @@
 const solve = require('advent_of_code_rs').solve;
 
 exports.handler = function(event, context, callback) {
-    const parameters = event.queryStringParameters;
-    const day = parseInt(parameters.day || '1');
-    const part = parseInt(parameters.part || '1')
-
-	console.log('About to log body');
-	console.log(event.body);
-
-    let input = '12';
-    const solution = solve(day, part, input);
-
-    callback(null, {
-        statusCode: 200,
-        body: "day=" + day + ", part=" + part + ", Solution: " + solution
+  if (event.httpMethod !== 'POST') {
+    return callback(null, {
+      statusCode: 400,
+      body: 'This endpoint only supports HTTP POST.',
+      headers: {
+        'Allow': 'POST'
+      }
     });
+  }
+
+  const parameters = event.queryStringParameters;
+  const day = parseInt(parameters.day || '1');
+  const part = parseInt(parameters.part || '1')
+  const input = event.body;
+
+  const solution = solve(day, part, input);
+
+  return callback(null, {
+    statusCode: 200,
+    body: "day=" + day + ", part=" + part + ", input=" + input + ", Solution: " + solution
+  });
 }
