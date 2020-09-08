43a7b7fd26f6ed0a53ee26242aa8ea9df81143b4
Fredrik Fornwall <fredrik@fornwall.net>
2020-09-08 12:36:11 +0200

    Introduce bump2version

--- crates/python/setup.py
+++ crates/python/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="advent_of_code_rs_python",
-    version="0.0.1",
+    version="2019.12.26",
     rust_extensions=[
         RustExtension("advent_of_code_rs_python", "Cargo.toml", binding=Binding.PyO3)
     ],
