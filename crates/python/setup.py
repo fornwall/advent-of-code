63dae02ee9a879d9aff948920cc317f432d455b0
Bump version workflow <bumpversion@users.noreply.github.com>
2020-09-10 10:30:54 +0000

    Bump version: 2019.12.41 → 2019.12.42

--- crates/python/setup.py
+++ crates/python/setup.py
@@ -12,7 +12,7 @@ setup(
     name="advent_of_code_rs_python",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="2019.12.41",
+    version="2019.12.42",
     rust_extensions=[
         RustExtension("advent_of_code_rs_python", "Cargo.toml", binding=Binding.PyO3)
     ],
