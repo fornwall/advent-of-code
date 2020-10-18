52e501948c19ac3e78fca63b5a23a7f2e5b02acb
Fredrik Fornwall <fredrik@fornwall.net>
2020-10-18 22:54:15 +0200

    Fix shell lint in reproduce-crashes.sh

--- crates/core/reproduce-crashes.sh
+++ crates/core/reproduce-crashes.sh
@@ -6,8 +6,8 @@ INPUT=$1
 for year in 2018 2019; do
   for day in {1..25}; do
     for part in 1 2; do
-      echo cargo run -q $year $day $part < $INPUT
-      cargo run -q $year $day $part < $INPUT > /dev/null
+      echo "cargo run -q $year $day $part < $INPUT"
+      cargo run -q $year $day $part < "$INPUT" > /dev/null
       EXIT_CODE=$?
       if [ $EXIT_CODE != 0 ] && [ $EXIT_CODE != 1 ]; then
         echo "Crash found"
