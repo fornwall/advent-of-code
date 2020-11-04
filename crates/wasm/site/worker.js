1b43ba6ecaf8ae4aacc7401f4de9cd0bfb906eb5
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-04 02:23:39 +0100

    Catch also fetch() exceptions

--- crates/wasm/site/worker.js
+++ crates/wasm/site/worker.js
@@ -15,34 +15,30 @@ async function run() {
   self.onmessage = async (message) => {
     const { year, day, part, input, wasm } = message.data;
     const startTime = performance.now();
-    if (wasm) {
-      if (!wasmWorking) {
-        postMessage({output: "Wasm is not working", isError: true});
-        return;
-      }
-      try {
+
+    try {
+      if (wasm) {
+        if (!wasmWorking) {
+          postMessage({output: "Wasm is not working", isError: true});
+          return;
+        }
         const output = wasm_bindgen.solve(year, day, part, input);
         const executionTime = performance.now() - startTime;
         postMessage({output, isError: false, wasm, executionTime});
-      } catch (error) {
-        console.log(error);
+      } else {
+        const response = await fetch(`https://aoc.fly.dev/solve/${year}/${day}/${part}/`, {
+          method: "POST",
+          headers: { "content-type": "text/plain" },
+          body: input
+        });
+        const responseText = await response.text();
         const executionTime = performance.now() - startTime;
-        postMessage({output: error.message, isError: true, wasm, executionTime});
+        postMessage({output: responseText, isError: !response.ok, wasm, executionTime});
       }
-    } else {
-      const response = await fetch(`https://aoc.fly.dev/solve/${year}/${day}/${part}/`, {
-        method: "POST",
-        headers: { "content-type": "text/plain" },
-        body: input
-      });
-      try {
-          const responseText = await response.text();
-          const executionTime = performance.now() - startTime;
-          postMessage({output: responseText, isError: !response.ok, wasm, executionTime});
-      } catch (error) {
-          const executionTime = performance.now() - startTime;
-          postMessage({output: error.message, isError: true, wasm, executionTime});
-      }
+    } catch (error) {
+      console.error(error);
+      const executionTime = performance.now() - startTime;
+      postMessage({output: error.message, isError: true, wasm, executionTime});
     }
   }
 }
