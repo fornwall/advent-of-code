4642843c7601aebf0b70a8faaf67b96420f9c04b
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-09 13:41:56 +0100

    Tweak page

--- crates/wasm/site/index.js
+++ crates/wasm/site/index.js
@@ -12,11 +12,15 @@ const output_element = document.getElementById('output');
 const api_execution_time = document.getElementById('api-execution-time');
 const wasm_execution_time = document.getElementById('wasm-execution-time');
 
+function disableButton(buttonToDisable) {
+  buttonToDisable.disabled = true;
+  buttonToDisable.classList.add('unusable');
+}
+
 worker.onmessage = (e) => {
   if ('wasmWorking' in e.data) {
     if (!e.data.wasmWorking) {
-      run_wasm_element.disabled = true;
-      run_wasm_element.classList.add('unusable');
+      disableButton(run_wasm_element);
       run_wasm_element.title = 'Wasm is not working - check console logs';
     }
   } else {
@@ -45,17 +49,14 @@ function showMessage(message, isError, wasm, executionTime) {
 }
 
 function execute(wasm) {
+  part_element.setCustomValidity((day_element.value == 25 && part_element.value == 2) ? 'Day 25 has no second part.' : '');
+
   if (document.querySelector("form").reportValidity()) {
     (wasm ? run_wasm_element : run_api_element).disabled = true;
     output_element.classList.remove('blink');
     const [year, day, part, input] = [year_element.value, day_element.value, part_element.value, input_element.value];
     worker.postMessage({ year, day, part, input, wasm });
   }
-}
-
-function updateInputLink() {
-  const link = `adventofcode.com/${year_element.value}/day/${day_element.value}/input`;
-  input_instructions_element.innerHTML = `(<a href="https://${link}">${link}</a>)`;
 }
 
 window.addEventListener('pageshow', () => {
@@ -72,7 +73,6 @@ window.addEventListener('pageshow', () => {
       }
     }
   }
-  updateInputLink();
 });
 
 async function clipboardMayWork() {
@@ -91,14 +91,18 @@ async function run() {
   run_wasm_element.addEventListener("click", (event) => execute(true));
 
   [year_element, day_element, part_element].forEach(element => element.addEventListener('input', () => {
-    updateInputLink();
     if (window.localStorage) {
       window.localStorage.setItem('problem', JSON.stringify({ year: year_element.value, day: day_element.value, part: part_element.value }));
     }
   }, false));
 
+  document.getElementById('open-input').addEventListener('click', () => {
+    const link = `https://adventofcode.com/${year_element.value}/day/${day_element.value}/input`;
+    window.open(link)
+  }, false);
+
+  const pasteButton = document.getElementById('paste');
   if (await clipboardMayWork()) {
-    const pasteButton = document.getElementById('paste');
     pasteButton.classList.remove('hidden');
     pasteButton.addEventListener('click', async () => {
       try {
@@ -107,10 +111,12 @@ async function run() {
         console.log(e);
       }
     }, false);
+  } else {
+    disableButton(pasteButton);
   }
 
+  const readFileButton = document.getElementById("read-file");
   if (window.showOpenFilePicker) {
-    const readFileButton = document.getElementById("read_file");
     readFileButton.classList.remove("hidden");
     readFileButton.addEventListener("click", async () => {
       try {
@@ -123,6 +129,8 @@ async function run() {
         console.log(e);
       }
     });
+  } else {
+    disableButton(readFileButton);
   }
 }
 
