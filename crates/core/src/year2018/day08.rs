56d95b22333e280ed406aa7d0bb39ee275888ecd
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-15 11:40:18 +0100

    Remove duplication in 2018-08

--- crates/core/src/year2018/day08.rs
+++ crates/core/src/year2018/day08.rs
@@ -1,82 +1,68 @@
-// Returns (size, metadata sum)
-fn count_metadata(data: &[i32], start: usize) -> Result<(usize, usize), String> {
+type InputNumber = u8;
+
+fn solution(input_string: &str, part1: bool) -> Result<usize, String> {
+    let data = input_string
+        .split_whitespace()
+        .map(|word| {
+            word.parse::<InputNumber>()
+                .map_err(|error| format!("Invalid input: {}", error.to_string()))
+        })
+        .collect::<Result<Vec<InputNumber>, _>>()?;
+    Ok(evaluate_node(&data, 0, part1)?.1)
+}
+
+fn evaluate_node(
+    data: &[InputNumber],
+    start: usize,
+    part1: bool,
+) -> Result<(usize, usize), String> {
     if data.len() < start + 2 {
         return Err("Invalid input".to_string());
     }
+
+    let mut children_values = Vec::new();
+    let mut offset_after_children = start + 2;
+
     let num_child_nodes = data[start];
-    let metadata_entries = data[start + 1] as usize;
-
-    let mut metadata_sum: usize = 0;
-    let mut current_index = start + 2;
     for _ in 0..num_child_nodes {
-        let child = count_metadata(data, current_index)?;
-        current_index = child.0;
-        metadata_sum += child.1;
+        let (offset_after_child, child_value) = evaluate_node(data, offset_after_children, part1)?;
+        offset_after_children = offset_after_child;
+        children_values.push(child_value);
     }
 
-    for i in data.iter().skip(current_index).take(metadata_entries) {
-        metadata_sum = metadata_sum
-            .checked_add(*i as usize)
-            .ok_or("Overflow in computation")?;
-    }
-
-    Ok((current_index + metadata_entries, metadata_sum as usize))
-}
-
-fn parse_input(input_string: &str) -> Result<Vec<i32>, String> {
-    input_string
-        .split_whitespace()
-        .map(|word| {
-            word.parse::<i32>()
-                .map_err(|error| format!("Invalid input: {}", error.to_string()))
+    let metadata_entries = data[start + 1] as usize;
+    let node_value = data
+        .iter()
+        .skip(offset_after_children)
+        .take(metadata_entries)
+        .map(|&metadata| {
+            if part1 || num_child_nodes == 0 {
+                metadata as usize
+            } else if metadata >= 1 && metadata as usize <= children_values.len() {
+                children_values[(metadata - 1) as usize]
+            } else {
+                0
+            }
         })
-        .collect()
+        .sum::<usize>()
+        + if part1 {
+            children_values.iter().sum()
+        } else {
+            0
+        };
+
+    let offset_after_current = offset_after_children
+        .checked_add(metadata_entries)
+        .ok_or("Overflow in computation")?;
+    Ok((offset_after_current, node_value as usize))
 }
 
 pub fn part1(input_string: &str) -> Result<usize, String> {
-    let data = parse_input(input_string)?;
-    Ok(count_metadata(&data, 0)?.1)
-}
-
-fn evaluate_node(data: &[i32], start: usize) -> Result<(usize, usize), String> {
-    if data.len() < start + 2 {
-        return Err("Invalid input".to_string());
-    }
-    let num_child_nodes = data[start];
-    let metadata_entries = data[start + 1] as usize;
-
-    let mut children_values = Vec::new();
-    let mut current_index = start + 2;
-
-    for _ in 0..num_child_nodes {
-        let child = evaluate_node(data, current_index)?;
-        current_index = child.0;
-        children_values.push(child.1);
-    }
-
-    let node_value =
-        data.iter()
-            .skip(current_index)
-            .take(metadata_entries)
-            .fold(0, |sum, &metadata| {
-                if num_child_nodes == 0 {
-                    sum + metadata as usize
-                } else if metadata >= 1 && metadata as usize <= children_values.len() {
-                    sum + children_values[(metadata - 1) as usize]
-                } else {
-                    sum
-                }
-            });
-
-    let new_index = current_index
-        .checked_add(metadata_entries)
-        .ok_or("Overflow in computation")?;
-    Ok((new_index, node_value as usize))
+    solution(input_string, true)
 }
 
 pub fn part2(input_string: &str) -> Result<usize, String> {
-    let data = parse_input(input_string)?;
-    Ok(evaluate_node(&data, 0)?.1)
+    solution(input_string, false)
 }
 
 #[test]
