82829dda4842286b45ed8d933c9b569a8efe4b52
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-21 00:55:59 +0100

    Add missing file

--- crates/wasm/site/show/worker-visualizer.js
+++ crates/wasm/site/show/worker-visualizer.js
@@ -1,8 +1,8 @@
 'use strict';
-self.importScripts("/show/advent_of_code_wasm.js");
+self.importScripts("advent_of_code_wasm.js");
 
 self.onmessage = async (message) => {
-    await wasm_bindgen("/show/advent_of_code_wasm_bg.wasm");
+    await wasm_bindgen("advent_of_code_wasm_bg.wasm");
 
     const { year, day, part, input } = message.data;
 
