a2562465829f5dbf616b37dabb431e3d90ba6771
Fredrik Fornwall <fredrik@fornwall.net>
2020-11-24 00:19:36 +0100

    Clean up CanvasRecorder.js

--- crates/wasm/site/show/CanvasRecorder.js
+++ crates/wasm/site/show/CanvasRecorder.js
@@ -1,94 +1,39 @@
-// CanvasRecorder.js - smusamashah
-// To record canvas effitiently using MediaRecorder
-// https://webrtc.github.io/samples/src/content/capture/canvas-record/
-export default function CanvasRecorder(canvas, video_bits_per_sec) {
-    this.start = startRecording;
-    this.stop = stopRecording;
-    this.save = download;
-
-    var recordedBlobs = [];
-    var supportedType = null;
-    var mediaRecorder = null;
-
-    var stream = canvas.captureStream();
-
-    const video = document.createElement('video');
-    video.style.display = 'none';
-
-    function startRecording() {
-        let types = [
+export default function CanvasRecorder(canvas, videoBitsPerSecond) {
+    this.start = () => {
+        const mimeType = [
+            'video/webm;codecs=vp9',
             "video/webm",
-            'video/webm,codecs=vp9',
             'video/vp8',
-            "video/webm\;codecs=vp8",
-            "video/webm\;codecs=daala",
-            "video/webm\;codecs=h264",
+            "video/webm;codecs=vp8",
+            "video/webm;codecs=daala",
+            "video/webm;codecs=h264",
             "video/mpeg"
-        ];
+        ].find(MediaRecorder.isTypeSupported);
 
-        for (let type of types) {
-            if (MediaRecorder.isTypeSupported(type)) {
-                supportedType = type;
-                break;
-            }
+        if (!mimeType) {
+            throw new Error("No supported mime type found for MediaRecorder");
         }
 
-        if (supportedType == null) {
-            console.error("No supported type found for MediaRecorder");
-        }
+        const stream = canvas.captureStream();
+        this.mediaRecorder = new MediaRecorder(stream, {
+            mimeType,
+            videoBitsPerSecond: videoBitsPerSecond || 5000000
+        });
 
-        const options = {
-            mimeType: supportedType,
-            videoBitsPerSecond: video_bits_per_sec || 2500000 // 2.5Mbps
-        };
-
-        recordedBlobs = [];
-        try {
-            mediaRecorder = new MediaRecorder(stream, options);
-        } catch (e) {
-            alert('MediaRecorder is not supported by this browser.');
-            console.error('Exception while creating MediaRecorder:', e);
-            return;
-        }
-
-        console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-        mediaRecorder.onstop = handleStop;
-        mediaRecorder.ondataavailable = handleDataAvailable;
-        mediaRecorder.start(100); // collect 100ms of data blobs
-        console.log('MediaRecorder started', mediaRecorder);
-    }
-
-    function handleDataAvailable(event) {
-        if (event.data && event.data.size > 0) {
-            recordedBlobs.push(event.data);
-        }
-    }
-
-    function handleStop(event) {
-        console.log('Recorder stopped: ', event);
-        const superBuffer = new Blob(recordedBlobs, { type: supportedType });
-        video.src = window.URL.createObjectURL(superBuffer);
-    }
-
-    function stopRecording() {
-        mediaRecorder.stop();
-        console.log('Recorded Blobs: ', recordedBlobs);
-        video.controls = true;
-    }
-
-    function download(file_name) {
-        const name = file_name || 'recording.webm';
-        const blob = new Blob(recordedBlobs, { type: supportedType });
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = name;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(() => {
-            document.body.removeChild(a);
+        this.mediaRecorder.ondataavailable = (event) => {
+            const url = window.URL.createObjectURL(event.data);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = this.fileName;
+            a.click();
             window.URL.revokeObjectURL(url);
-        }, 100);
+        }
+
+        this.mediaRecorder.start();
+    }
+
+    this.stopAndSave = (fileName) => {
+        this.fileName = fileName || 'recording.webm';
+        this.mediaRecorder.stop();
     }
 }
