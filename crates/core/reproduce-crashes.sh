51e1ca7c82de699555f2f5385e3a3c2c7341ca25
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-05 00:01:57 +0100

    Skip initial bytes in reproduce-crashes.txt

--- crates/core/reproduce-crashes.sh
+++ crates/core/reproduce-crashes.sh
@@ -6,8 +6,8 @@ INPUT=$1
 for year in 2017 2018 2019 2020; do
   for day in {1..25}; do
     for part in 1 2; do
-      echo "cargo run -q $year $day $part < $INPUT"
-      cargo run -q $year $day $part < "$INPUT" > /dev/null
+      echo "tail -c +4 $INPUT | cargo run -q $year $day $part"
+      tail -c +4 $INPUT | cargo run -q $year $day $part > /dev/null
       EXIT_CODE=$?
       if [ $EXIT_CODE != 0 ] && [ $EXIT_CODE != 1 ]; then
         echo "Crash found"
