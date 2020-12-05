4fc7a4cb44e6ce2758b6ab1864b5bbbc3b0d8309
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-06 00:09:29 +0100

    Remove commented out code

--- crates/core/src/year2020/day05.rs
+++ crates/core/src/year2020/day05.rs
@@ -40,20 +40,6 @@ pub fn solve(input: &mut Input) -> Result<u16, String> {
             }
         }
         Err("No gap found".to_string())
-        /*
-        let mut seat_ids = seat_ids.collect::<Vec<_>>();
-        seat_ids.sort_unstable();
-        let non_adjacent_seats = seat_ids
-            .windows(2)
-            // "It's a completely full flight, so your seat should be the only missing boarding pass in your list":
-            .filter(|two_seats| two_seats[0] + 1 != two_seats[1])
-            .collect::<Vec<_>>();
-        if non_adjacent_seats.len() == 1 {
-            Ok(non_adjacent_seats[0][0] + 1)
-        } else {
-            Err("No unique gap found".to_string())
-        }
-         */
     }
 }
 
