cd8c2e0ce42a935aa557420a09ab1a2558857fbf
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-06 16:20:05 +0100

    Add 2020-05

--- crates/core/src/year2020/day06.rs
+++ crates/core/src/year2020/day06.rs
@@ -1,18 +1,65 @@
 use crate::input::Input;
 
-pub fn solve(input: &mut Input) -> Result<u32, String> {
-    Ok(input.text.len() as u32)
+type AnswersBitSet = u32;
+
+/// Convert the yes answers of a person, as a string of a-z characters identifying
+/// the questions the person answered yes to, to a bit set where:
+/// - First bit is set if question 'a' has been answered with yes.
+/// - Second bit is set if question 'b' has been answered with yes.
+/// - ...
+fn person_answers_to_bit_set(answers: &str) -> AnswersBitSet {
+    answers
+        .bytes()
+        .map(|char| 1 << (char - b'a'))
+        .sum::<AnswersBitSet>()
+}
+
+pub fn solve(input: &mut Input) -> Result<AnswersBitSet, String> {
+    const GROUP_SEPARATOR: &str = "\n\n";
+
+    if !input
+        .text
+        .bytes()
+        .all(|b| matches!(b, b'a'..=b'z' | b'\r' | b'\n'))
+    {
+        return Err("Invalid input - only a-z, \r and \n expected".to_string());
+    }
+
+    let initial_bit_set = input.part_values(0, AnswersBitSet::MAX);
+
+    let bit_set_merger = |answers_set, person_answers| {
+        input.part_values(answers_set | person_answers, answers_set & person_answers)
+    };
+
+    let computer = |text: &str| {
+        Ok(text
+            .split(GROUP_SEPARATOR)
+            .map(|group_answers| {
+                group_answers
+                    .lines()
+                    .map(person_answers_to_bit_set)
+                    .fold(initial_bit_set, bit_set_merger)
+                    .count_ones()
+            })
+            .sum())
+    };
+
+    if input.text.contains('\r') {
+        // Only call replace() (which results in memory allocation) if necessary.
+        computer(&input.text.replace('\r', ""))
+    } else {
+        computer(input.text)
+    }
 }
 
 #[test]
 pub fn tests() {
-    use crate::test_part_one; // , test_part_one_error, test_part_two, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
 
-    test_part_one!("" => 0);
+    test_part_one!("abc\n\nabc" => 6);
+    test_part_one!("abc\r\n\r\nabc" => 6);
 
-    // test_part_one_error!("1- b: asdf" => "Line 1: Invalid format - expected '$START-$END $CHAR: $PASSWORD'");
-    // test_part_two_error!("1-3 a: asdf\nhi\n" => "Line 2: Invalid format - expected '$START-$END $CHAR: $PASSWORD'");
-
-    // let real_input = include_str!("day05_input.txt");
-    // test_part_one!(real_input => 636);
+    let real_input = include_str!("day06_input.txt");
+    test_part_one!(real_input => 6686);
+    test_part_two!(real_input => 3476);
 }
