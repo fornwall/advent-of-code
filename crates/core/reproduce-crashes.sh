293a92a74220c5e700414375a44a481b9731c473
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-10 10:49:33 +0100

    Improve reproduce-crashes.sh script

--- crates/core/reproduce-crashes.sh
+++ crates/core/reproduce-crashes.sh
@@ -6,8 +6,10 @@ INPUT=$1
 for year in {2017..2020}; do
   for day in {1..25}; do
     for part in 1 2; do
-      echo "tail -c +4 $INPUT | cargo run -q $year $day $part"
-      tail -c +4 $INPUT | cargo run -q $year $day $part > /dev/null
+      COMMAND="tail -c +4 \"$INPUT\" | RUST_BACKTRACE=1 cargo run -q $year $day $part"
+      echo $COMMAND
+      #tail -c +4 $INPUT | cargo run -q $year $day $part > /dev/null
+      sh -c "$COMMAND" > /dev/null
       EXIT_CODE=$?
       if [ $EXIT_CODE != 0 ] && [ $EXIT_CODE != 1 ]; then
         echo "Crash found"
