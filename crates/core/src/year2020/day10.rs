e18f778e57af86eafaf9672431295ec3bdd213e7
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-10 10:13:33 +0100

    Polish 2020-10

--- crates/core/src/year2020/day10.rs
+++ crates/core/src/year2020/day10.rs
@@ -1,51 +1,50 @@
 use crate::input::Input;
 
-pub fn solve(input: &mut Input) -> Result<u64, String> {
-    const MAX_DIFF: u64 = 3;
+type JoltageAmount = u64;
+
+pub fn solve(input: &mut Input) -> Result<JoltageAmount, String> {
+    const MAX_DIFF: JoltageAmount = 3;
 
     let mut joltages = std::iter::once(Ok(0))
         .chain(input.text.lines().enumerate().map(|(line_idx, line)| {
-            line.parse::<u64>().map_err(|parse_error| {
+            line.parse::<JoltageAmount>().map_err(|parse_error| {
                 format!("Line {}: Invalid joltage - {}", line_idx + 1, parse_error)
             })
         }))
-        .collect::<Result<Vec<u64>, _>>()?;
+        .collect::<Result<Vec<_>, _>>()?;
 
     joltages.sort_unstable();
     joltages.push(joltages[joltages.len() - 1] + MAX_DIFF);
 
     if input.is_part_one() {
-        let diff_counts =
-            joltages
-                .windows(2)
-                .try_fold([0; MAX_DIFF as usize + 1], |mut acc, window| {
-                    let diff = window[1] - window[0];
-                    if diff > MAX_DIFF {
-                        Err(format!(
-                            "Too big difference between adapters - cannot go from {} to {}",
-                            window[0], window[1]
-                        ))
-                    } else {
-                        acc[diff as usize] += 1;
-                        Ok(acc)
-                    }
-                })?;
-        Ok(diff_counts[1] * diff_counts[3])
+        joltages
+            .windows(2)
+            .try_fold([0; MAX_DIFF as usize + 1], |mut diff_count, window| {
+                let diff = window[1] - window[0];
+                if diff > MAX_DIFF {
+                    Err(format!(
+                        "Too big difference between adapters - cannot go from {} to {}",
+                        window[0], window[1]
+                    ))
+                } else {
+                    diff_count[diff as usize] += 1;
+                    Ok(diff_count)
+                }
+            })
+            .map(|diff_counts| diff_counts[1] * diff_counts[3])
     } else {
         let mut distinct_ways_counts = vec![0; joltages.len()];
         distinct_ways_counts[0] = 1;
 
-        for (idx, joltage) in joltages.iter().enumerate().skip(1) {
-            let mut back_step = 1;
-            while idx >= back_step {
-                let back_idx = idx - back_step;
-                let joltage_diff = joltage - joltages[back_idx];
-                if joltage_diff <= MAX_DIFF {
-                    distinct_ways_counts[idx] += distinct_ways_counts[back_idx];
-                } else {
-                    break;
-                }
-                back_step += 1;
+        for (idx, joltage) in joltages.iter().enumerate() {
+            let this_distinct_count = distinct_ways_counts[idx];
+
+            for (offset, _) in joltages[(idx + 1)..]
+                .iter()
+                .take_while(|&higher_joltage| higher_joltage - joltage <= MAX_DIFF)
+                .enumerate()
+            {
+                distinct_ways_counts[idx + offset + 1] += this_distinct_count;
             }
         }
 
@@ -60,8 +59,9 @@ pub fn tests() {
     let example = "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4";
     test_part_one!(example => 35);
     test_part_two!(example => 8);
+    test_part_two!("1" => 1);
 
     let real_input = include_str!("day10_input.txt");
     test_part_one!(real_input => 2376);
-    test_part_two!(real_input => 129586085429248);
+    test_part_two!(real_input => 129_586_085_429_248);
 }
