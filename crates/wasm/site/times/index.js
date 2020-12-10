d76c809a3784ddf57d7bfe953df309df8d8de4c1
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-10 19:52:39 +0100

    Fix javascript style

--- crates/wasm/site/times/index.js
+++ crates/wasm/site/times/index.js
@@ -2,42 +2,42 @@ const currentYear = new URLSearchParams(window.location.search).get('year') || '
 document.querySelector('h1').textContent = `Advent of Code ${currentYear} timings`;
 
 async function run() {
-  const baselineResponse = await fetch("https://fornwall.net/baseline.json");
+  const baselineResponse = await fetch('https://fornwall.net/baseline.json');
   const baselineJson = await baselineResponse.json();
   const benchmarks = Object.entries(baselineJson['benchmarks']);
   const extractMeanTime = (info) => info['criterion_estimates_v1']['mean']['point_estimate'];
   let totalRuntime = 0;
 
-  const part_1s = {
+  const part1s = {
     x: [],
     y: [],
     type: 'bar',
-    name: 'Part 1'
+    name: 'Part 1',
   };
-  const part_2s = {
+  const part2s = {
     x: [],
     y: [],
     type: 'bar',
-    name: 'Part 2'
+    name: 'Part 2',
   };
   for (const [benchmarkName, benchmarkInfo] of benchmarks) {
      if (!benchmarkName.startsWith(currentYear)) continue;
-     const [year, day, part] = benchmarkName.split('_');
+     const [, day, part] = benchmarkName.split('_');
      const meanTime = extractMeanTime(benchmarkInfo);
      totalRuntime += meanTime;
      const runtimeMicroSeconds = Math.round(meanTime / 1000);
-     const trace = (part == '1') ? part_1s : part_2s;
+     const trace = (part == '1') ? part1s : part2s;
      trace.x.push(`Day ${day}`);
      trace.y.push(runtimeMicroSeconds);
   }
   const barLayout = {
-      title: "Execution time per day and part",
+      title: 'Execution time per day and part',
       barmode: 'group',
       yaxis: {
-        title: 'Execution time (microseconds)'
-      }
+        title: 'Execution time (microseconds)',
+      },
   };
-  Plotly.newPlot('firstPlot', [part_1s, part_2s], barLayout);
+  Plotly.newPlot('firstPlot', [part1s, part2s], barLayout);
 
   const pieData = {
       values: [],
@@ -46,19 +46,19 @@ async function run() {
   };
   for (const [benchmarkName, benchmarkInfo] of benchmarks) {
      if (!benchmarkName.startsWith(currentYear)) continue;
-     let [year, day, part] = benchmarkName.split('_');
+     let [, day, part] = benchmarkName.split('_');
      let percentageRuntime = 100. * extractMeanTime(benchmarkInfo) / totalRuntime;
      pieData.labels.push(`Day ${day}, Part ${part}`);
      pieData.values.push(percentageRuntime);
   }
   const pieLayout = {
-    title: `Total: ${Number(Math.round(totalRuntime / 1000.)).toLocaleString()} microseconds`
+    title: `Total: ${Number(Math.round(totalRuntime / 1000.)).toLocaleString()} microseconds`,
   };
   Plotly.newPlot('secondPlot', [pieData], pieLayout);
 
-  window.addEventListener("resize", () => {
-    Plotly.Plots.resize(document.getElementById("firstPlot"));
-    Plotly.Plots.resize(document.getElementById("secondPlot"));
+  window.addEventListener('resize', () => {
+    Plotly.Plots.resize(document.getElementById('firstPlot'));
+    Plotly.Plots.resize(document.getElementById('secondPlot'));
   });
 }
 run();
