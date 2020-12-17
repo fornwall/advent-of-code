8ad03be1fb91d8ebef20b13506b524102bb91af0
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-17 10:50:06 +0100

    Speed up 2020-17

--- crates/core/src/year2020/day17.rs
+++ crates/core/src/year2020/day17.rs
@@ -1,5 +1,6 @@
 use crate::input::Input;
-use std::collections::HashSet;
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
 use std::ops::RangeInclusive;
 
 type CoordinateComponent = i8;
@@ -14,14 +15,12 @@ struct Coordinate {
 
 struct Grid {
     occupied_coordinates: HashSet<Coordinate>,
-    checked: HashSet<Coordinate>,
-    to_add: Vec<Coordinate>,
-    to_remove: Vec<Coordinate>,
+    active_neighbors_count: HashMap<Coordinate, u8>,
 }
 
 impl Grid {
     fn parse(input: &str) -> Result<Self, String> {
-        let mut occupied_coordinates = HashSet::new();
+        let mut occupied_coordinates = HashSet::with_capacity(2000);
 
         for (row, line) in input.lines().enumerate() {
             for (col, b) in line.bytes().enumerate() {
@@ -38,55 +37,12 @@ impl Grid {
 
         Ok(Self {
             occupied_coordinates,
-            checked: HashSet::new(),
-            to_add: Vec::new(),
-            to_remove: Vec::new(),
+            active_neighbors_count: HashMap::with_capacity(25000),
         })
     }
 
-    fn active_next_cycle(
-        &self,
-        is_occupied: bool,
-        coordinate: Coordinate,
-        w_range: &RangeInclusive<CoordinateComponent>,
-    ) -> bool {
-        let mut active_neighbors = 0;
-
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                for dz in -1..=1 {
-                    for dw in w_range.clone() {
-                        if !(dx == 0 && dy == 0 && dz == 0 && dw == 0) {
-                            let coordinate = Coordinate {
-                                x: coordinate.x + dx,
-                                y: coordinate.y + dy,
-                                z: coordinate.z + dz,
-                                w: coordinate.w + dw,
-                            };
-                            if self.occupied_coordinates.contains(&coordinate) {
-                                active_neighbors += 1;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if is_occupied {
-            // - If a cube is active and exactly 2 or 3 of its neighbors are also active,
-            // the cube remains active. Otherwise, the cube becomes inactive.
-            active_neighbors == 2 || active_neighbors == 3
-        } else {
-            // - If a cube is inactive but exactly 3 of its neighbors are active, the cube
-            // becomes active. Otherwise, the cube remains inactive.
-            active_neighbors == 3
-        }
-    }
-
     fn cycle(&mut self, w_range: &RangeInclusive<CoordinateComponent>) {
-        self.checked.clear();
-        self.to_remove.clear();
-        self.to_add.clear();
+        self.active_neighbors_count.clear();
 
         for coordinate in self.occupied_coordinates.iter() {
             for dx in -1..=1 {
@@ -100,15 +56,18 @@ impl Grid {
                                 w: coordinate.w + dw,
                             };
 
-                            if self.checked.insert(coordinate) {
-                                let is_active = self.occupied_coordinates.contains(&coordinate);
-                                let will_be_active =
-                                    self.active_next_cycle(is_active, coordinate, w_range);
+                            let value_to_add = if dx == 0 && dy == 0 && dz == 0 && dw == 0 {
+                                0
+                            } else {
+                                1
+                            };
 
-                                if is_active && !will_be_active {
-                                    self.to_remove.push(coordinate);
-                                } else if will_be_active && !is_active {
-                                    self.to_add.push(coordinate);
+                            match self.active_neighbors_count.entry(coordinate) {
+                                Entry::Occupied(mut entry) => {
+                                    entry.insert(entry.get() + value_to_add);
+                                }
+                                Entry::Vacant(entry) => {
+                                    entry.insert(value_to_add);
                                 }
                             }
                         }
@@ -117,9 +76,22 @@ impl Grid {
             }
         }
 
-        self.occupied_coordinates.extend(self.to_add.iter());
-        for remove in self.to_remove.iter() {
-            self.occupied_coordinates.remove(remove);
+        for (&coordinate, &active_neighbors) in self.active_neighbors_count.iter() {
+            let is_active = self.occupied_coordinates.contains(&coordinate);
+            let will_be_active = if is_active {
+                // - If a cube is active and exactly 2 or 3 of its neighbors are also active,
+                // the cube remains active. Otherwise, the cube becomes inactive.
+                active_neighbors == 2 || active_neighbors == 3
+            } else {
+                // - If a cube is inactive but exactly 3 of its neighbors are active, the cube
+                // becomes active. Otherwise, the cube remains inactive.
+                active_neighbors == 3
+            };
+            if is_active && !will_be_active {
+                self.occupied_coordinates.remove(&coordinate);
+            } else if !is_active && will_be_active {
+                self.occupied_coordinates.insert(coordinate);
+            }
         }
     }
 }
