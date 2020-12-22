2a3202d7a6320c716226ad69827dcfee321f1629
Fredrik Fornwall <fredrik@fornwall.net>
2020-12-22 16:26:13 +0100

    2020-22

--- crates/core/src/year2020/day22.rs
+++ crates/core/src/year2020/day22.rs
@@ -1,45 +1,109 @@
 use crate::input::Input;
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
-fn parse_player(input: &str) -> VecDeque<u8> {
-    let mut result = VecDeque::new();
-    for line in input.lines().skip(1) {
-        result.push_back(line.parse::<u8>().unwrap());
+enum Winner {
+    Player1,
+    Player2,
+}
+
+fn parse_player_cards(input: &str) -> Option<VecDeque<u8>> {
+    input
+        .lines()
+        .skip(1)
+        .map(str::parse)
+        .collect::<Result<_, _>>()
+        .ok()
+}
+
+fn play_recursive(player_1_cards: &mut VecDeque<u8>, player_2_cards: &mut VecDeque<u8>) -> Winner {
+    #![allow(clippy::unwrap_used)]
+    let mut seen = HashSet::new();
+
+    loop {
+        if !seen.insert((player_1_cards.clone(), player_2_cards.clone())) {
+            return Winner::Player1;
+        } else if player_1_cards.is_empty() {
+            return Winner::Player2;
+        } else if player_2_cards.is_empty() {
+            return Winner::Player1;
+        }
+
+        let c1 = player_1_cards.pop_front().unwrap();
+        let c2 = player_2_cards.pop_front().unwrap();
+
+        let winner = if player_1_cards.len() >= c1 as usize && player_2_cards.len() >= c2 as usize {
+            let mut p1_subgame_cards = player_1_cards
+                .iter()
+                .take(c1 as usize)
+                .copied()
+                .collect::<VecDeque<u8>>();
+            let mut p2_subgame_cards = player_2_cards
+                .iter()
+                .take(c2 as usize)
+                .copied()
+                .collect::<VecDeque<u8>>();
+            play_recursive(&mut p1_subgame_cards, &mut p2_subgame_cards)
+        } else if c1 > c2 {
+            Winner::Player1
+        } else {
+            Winner::Player2
+        };
+
+        match winner {
+            Winner::Player1 => {
+                player_1_cards.push_back(c1);
+                player_1_cards.push_back(c2);
+            }
+            Winner::Player2 => {
+                player_2_cards.push_back(c2);
+                player_2_cards.push_back(c1);
+            }
+        }
     }
-    result
 }
 
 pub fn solve(input: &mut Input) -> Result<u64, String> {
+    #![allow(clippy::unwrap_used)]
     let on_error = || "Invalid input".to_string();
-    let mut parts = input.text.splitn(2, "\n\n");
-    let mut p1 = parse_player(parts.next().ok_or_else(on_error)?);
-    let mut p2 = parse_player(parts.next().ok_or_else(on_error)?);
-    assert_eq!(p1.len(), p2.len());
 
-    if input.is_part_one() {
-        while p1.len() > 0 && p2.len() > 0 {
-            let c1 = p1.pop_front().unwrap();
-            let c2 = p2.pop_front().unwrap();
+    let mut parts = input.text.splitn(2, "\n\n");
+    let mut player_1_cards =
+        parse_player_cards(parts.next().ok_or_else(on_error)?).ok_or_else(on_error)?;
+    let mut player_2_cards =
+        parse_player_cards(parts.next().ok_or_else(on_error)?).ok_or_else(on_error)?;
+
+    let winner = if input.is_part_one() {
+        while !player_1_cards.is_empty() && !player_2_cards.is_empty() {
+            let c1 = player_1_cards.pop_front().unwrap();
+            let c2 = player_2_cards.pop_front().unwrap();
             if c1 > c2 {
-                p1.push_back(c1);
-                p1.push_back(c2);
+                player_1_cards.push_back(c1);
+                player_1_cards.push_back(c2);
             } else {
-                p2.push_back(c2);
-                p2.push_back(c1);
+                player_2_cards.push_back(c2);
+                player_2_cards.push_back(c1);
             }
         }
-
-        let mut winner = if p1.is_empty() { p2 } else { p1 };
-        let mut result = 0_u64;
-        let mut i = 1_u64;
-        while let Some(c) = winner.pop_back() {
-            result += i * u64::from(c);
-            i += 1;
+        if player_1_cards.is_empty() {
+            Winner::Player2
+        } else {
+            Winner::Player1
         }
-        Ok(result)
     } else {
-        Ok(0)
-    }
+        play_recursive(&mut player_1_cards, &mut player_2_cards)
+    };
+
+    let winner_cards = match winner {
+        Winner::Player1 => player_1_cards,
+        Winner::Player2 => player_2_cards,
+    };
+
+    Ok(winner_cards
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(idx, &card)| (idx + 1) as u64 * u64::from(card))
+        .sum())
 }
 
 #[test]
@@ -61,9 +125,9 @@ Player 2:
 10
 ";
     test_part_one!(example => 306);
-    test_part_two!(example  => 291);
+    test_part_two!(example => 291);
 
     let real_input = include_str!("day22_input.txt");
     test_part_one!(real_input => 33098);
-    // test_part_two!(real_input => 0);
+    test_part_two!(real_input => 35055);
 }
