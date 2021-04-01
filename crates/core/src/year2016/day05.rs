12009d406114bbb45ed16e58f5573d68fdef20dc
Fredrik Fornwall <fredrik@fornwall.net>
2021-04-01 23:56:12 +0200

    2016, day 5 and 6

--- crates/core/src/year2016/day05.rs
+++ crates/core/src/year2016/day05.rs
@@ -1,14 +1,63 @@
 use crate::Input;
+use md5::digest::generic_array::arr;
+use md5::digest::FixedOutput;
+use md5::{Digest, Md5};
+use std::iter::FromIterator;
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+pub fn solve(input: &mut Input) -> Result<String, String> {
+    const MAX_INDEX: i32 = 100_000_000;
+
+    let mut password = input.part_values(Vec::new(), vec![' '; 8]);
+    let door_id = input.text.as_bytes();
+
+    let mut hasher = Md5::new();
+    let mut output = arr![u8; 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0];
+
+    for index in 0..MAX_INDEX {
+        hasher.update(door_id);
+        hasher.update(index.to_string().as_bytes());
+
+        hasher.finalize_into_reset(&mut output);
+
+        // Check if hash starts with five zeros without converting it to a string:
+        if output[..2] == [0, 0] && output[2] <= 0x0F {
+            if input.is_part_one() {
+                password.push(
+                    format!("{:x?}", (output[2] & 0x0F_u8))
+                        .as_str()
+                        .chars()
+                        .next()
+                        .unwrap_or('_'),
+                );
+                if password.len() == 8 {
+                    return Ok(String::from_iter(password));
+                }
+            } else {
+                let position = output[2] & 0x0F_u8;
+                let character = output[3] >> 4;
+                // "Use only the first result for each position, and ignore invalid positions.":
+                if position < 8 && password[position as usize] == ' ' {
+                    password[position as usize] =
+                        format!("{:x?}", character).chars().next().unwrap_or('_');
+                    if !password.contains(&' ') {
+                        return Ok(String::from_iter(password));
+                    }
+                }
+            }
+        }
+    }
+
+    return Err(format!("Aborting after {} iterations", MAX_INDEX));
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
+
+    test_part_one!("abc" => "18f47a30".to_string());
+    test_part_two!("abc" => "05ace8e3".to_string());
 
     let real_input = include_str!("day05_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => "1a3099aa".to_string());
+    test_part_two!(real_input => "694190cd".to_string());
 }
--- crates/core/src/year2016/day06.rs
+++ crates/core/src/year2016/day06.rs
@@ -1,14 +1,48 @@
 use crate::Input;
+use std::collections::HashMap;
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+pub fn solve(input: &mut Input) -> Result<String, String> {
+    let mut counts: [HashMap<u8, u8>; 8] = [
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+    ];
+
+    for line in input.text.lines() {
+        for (index, c) in line.bytes().take(counts.len()).enumerate() {
+            let count: &mut HashMap<u8, u8> = &mut counts[index];
+            *count.entry(c).or_insert(0) += 1;
+        }
+    }
+
+    Ok(counts
+        .iter()
+        .map(|count| {
+            count
+                .iter()
+                .max_by(|a, b| {
+                    if input.is_part_one() {
+                        a.1.cmp(b.1)
+                    } else {
+                        b.1.cmp(a.1)
+                    }
+                })
+                .map(|(&key, _value)| key as char)
+                .unwrap_or('?')
+        })
+        .collect())
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
 
     let real_input = include_str!("day06_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => "qzedlxso".to_string());
+    test_part_two!(real_input => "ucmifjae".to_string());
 }
