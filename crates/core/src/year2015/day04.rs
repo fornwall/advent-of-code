ffcd097b13e947c436c54460c4a3635c7be4adcd
Fredrik Fornwall <fredrik@fornwall.net>
2021-04-02 00:45:42 +0200

    2015, day 4 and 5

--- crates/core/src/year2015/day04.rs
+++ crates/core/src/year2015/day04.rs
@@ -1,14 +1,36 @@
 use crate::Input;
+use md5::digest::generic_array::arr;
+use md5::digest::FixedOutput;
+use md5::{Digest, Md5};
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    const MAX_INDEX: i32 = 100_000_000;
+
+    let secret_key = input.text.as_bytes();
+
+    let mut hasher = Md5::new();
+    let mut output = arr![u8; 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0];
+
+    for index in 0..MAX_INDEX {
+        hasher.update(secret_key);
+        hasher.update(index.to_string().as_bytes());
+
+        hasher.finalize_into_reset(&mut output);
+
+        // Check if hash starts with five/six zeros without converting it to a string:
+        if output[..2] == [0, 0] && output[2] <= input.part_values(0x0F, 0) {
+            return Ok(index as u32);
+        }
+    }
+
+    return Err(format!("Aborting after {} iterations", MAX_INDEX));
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
 
     let real_input = include_str!("day04_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => 117_946);
+    test_part_two!(real_input => 3_938_038);
 }
--- crates/core/src/year2015/day05.rs
+++ crates/core/src/year2015/day05.rs
@@ -1,14 +1,89 @@
 use crate::Input;
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+pub fn solve(input: &mut Input) -> Result<usize, String> {
+    fn is_nice_part_1(string: &&str) -> bool {
+        let mut vowel_count = 0;
+        let mut twice_in_a_row = false;
+        let mut contains_forbidden = false;
+        let mut last_char = ' ';
+
+        for c in string.chars() {
+            if "aeiou".contains(c) {
+                vowel_count += 1;
+            }
+
+            if c == last_char {
+                twice_in_a_row = true;
+            }
+
+            // "It does not contain the strings ab, cd, pq, or xy,
+            // even if they are part of one of the other requirements":
+            if (last_char == 'a' && c == 'b')
+                || (last_char == 'c' && c == 'd')
+                || (last_char == 'p' && c == 'q')
+                || (last_char == 'x' && c == 'y')
+            {
+                contains_forbidden = true;
+            }
+
+            last_char = c;
+        }
+
+        !contains_forbidden && vowel_count >= 3 && twice_in_a_row
+    }
+
+    fn is_nice_part_2(string: &&str) -> bool {
+        fn find_subsequence(haystack: &[u8], needle: &[u8]) -> bool {
+            haystack
+                .windows(needle.len())
+                .any(|window| window == needle)
+        }
+
+        let bytes = string.as_bytes();
+
+        let repeated_with_one_letter_between =
+            bytes.windows(3).any(|window| window[0] == window[2]);
+        if !repeated_with_one_letter_between {
+            return false;
+        }
+
+        for i in 0..bytes.len() - 3 {
+            let this_char = bytes[i];
+            let next_char = bytes[i + 1];
+            if find_subsequence(&bytes[(i + 2)..], &[this_char, next_char]) {
+                return true;
+            }
+        }
+        false
+    }
+
+    Ok(input
+        .text
+        .lines()
+        .filter(if input.is_part_one() {
+            is_nice_part_1
+        } else {
+            is_nice_part_2
+        })
+        .count())
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
+
+    test_part_one!("ugknbfddgicrmopn" => 1);
+    test_part_one!("aaa" => 1);
+    test_part_one!("jchzalrnumimnmhp" => 0);
+    test_part_one!("haegwjzuvuyypxyu" => 0);
+    test_part_one!("dvszwmarrgswjxmb" => 0);
+
+    test_part_two!("qjhvhtzxzqqjkmpb" => 1);
+    test_part_two!("xxyxx" => 1);
+    test_part_two!("uurcxstgmygtbstg" => 0);
+    test_part_two!("ieodomkazucvgmuy" => 0);
 
     let real_input = include_str!("day05_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => 238);
+    test_part_two!(real_input => 69);
 }
