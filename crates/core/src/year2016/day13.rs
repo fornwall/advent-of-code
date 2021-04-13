98036b8ea16cf4b2cad12239bd4a122a3bfe4c0e
Fredrik Fornwall <fredrik@fornwall.net>
2021-04-13 16:21:18 +0200

    2016, day 13

--- crates/core/src/year2016/day13.rs
+++ crates/core/src/year2016/day13.rs
@@ -1,14 +1,84 @@
 use crate::Input;
+use std::cmp::Reverse;
+use std::collections::hash_map::Entry;
+use std::collections::{BinaryHeap, HashMap};
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+const fn is_wall(x: i32, y: i32, magic_number: i32) -> bool {
+    let sum = x * x + 3 * x + 2 * x * y + y + y * y + magic_number;
+    sum.count_ones() % 2 == 1
+}
+
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    const TARGET: (i32, i32) = (31, 39);
+
+    let magic_number = input
+        .text
+        .parse::<i32>()
+        .map_err(|e| format!("Invalid magic number: {}", e))?;
+
+    let mut to_visit = BinaryHeap::new();
+    let mut cost_of_states = HashMap::new();
+
+    let initial_state = (1, 1);
+    let mut visit_count = 0;
+
+    to_visit.push(Reverse((0, 0, initial_state)));
+    cost_of_states.insert(initial_state, 0);
+
+    while let Some(Reverse((_estimate, visited_state_cost, visited_state))) = to_visit.pop() {
+        if input.is_part_one() {
+            if visited_state == TARGET {
+                return Ok(visited_state_cost);
+            }
+        } else if visited_state_cost > 50 {
+            return Ok(visit_count);
+        } else {
+            visit_count += 1;
+        }
+
+        for &(diffx, diffy) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            let new_x = visited_state.0 + diffx;
+            let new_y = visited_state.1 + diffy;
+            if new_x < 0 || new_y < 0 || is_wall(new_x, new_y, magic_number) {
+                continue;
+            }
+
+            let new_cost = visited_state_cost + 1;
+
+            let do_insert = match cost_of_states.entry((new_x, new_y)) {
+                Entry::Vacant(entry) => {
+                    entry.insert(new_cost);
+                    true
+                }
+                Entry::Occupied(mut entry) => {
+                    if new_cost < *entry.get() {
+                        entry.insert(new_cost);
+                        true
+                    } else {
+                        false
+                    }
+                }
+            };
+            if do_insert {
+                let new_estimate = new_cost
+                    + if input.is_part_one() {
+                        (new_x - TARGET.0).abs() as u32 + (new_y - TARGET.1).abs() as u32
+                    } else {
+                        0
+                    };
+                to_visit.push(Reverse((new_estimate, new_cost, (new_x, new_y))));
+            }
+        }
+    }
+
+    Err("No solution found".to_string())
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
 
     let real_input = include_str!("day13_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => 82);
+    test_part_two!(real_input => 138);
 }
