8e2ca84e77ab58c6e33534ab2b43156ac9b3472a
Fredrik Fornwall <fredrik@fornwall.net>
2021-04-19 23:46:55 +0200

    2015: Day 24 and 25

--- crates/core/src/year2015/day24.rs
+++ crates/core/src/year2015/day24.rs
@@ -1,14 +1,75 @@
 use crate::Input;
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+// arr[]  ---> Input Array
+// data[] ---> Temporary array to store current combination
+// start & end ---> Staring and Ending indexes in arr[]
+// index  ---> Current index in data[]
+// r ---> Size of a combination to be printed */
+fn visit_subset_internal<F>(
+    input: &[u8],
+    output: &mut [u8],
+    input_idx: usize,
+    output_idx: usize,
+    on_subset: &mut F,
+) where
+    F: FnMut(&[u8]),
+{
+    if output_idx == output.len() {
+        on_subset(output);
+        return;
+    } else if input_idx >= input.len() {
+        return;
+    }
+
+    // Include input[input_idx]:
+    output[output_idx] = input[input_idx];
+    visit_subset_internal(input, output, input_idx + 1, output_idx + 1, on_subset);
+    // Skip input[input_idx]:
+    visit_subset_internal(input, output, input_idx + 1, output_idx, on_subset);
+}
+
+fn visit_subsets<F>(input: &[u8], subset_size: usize, on_subsete: &mut F)
+where
+    F: FnMut(&[u8]),
+{
+    let mut output = vec![0; subset_size];
+    visit_subset_internal(input, &mut output, 0, 0, on_subsete);
+}
+
+pub fn solve(input: &mut Input) -> Result<u128, String> {
+    let weights = input
+        .text
+        .lines()
+        .map(str::parse::<u8>)
+        .collect::<Result<Vec<u8>, _>>()
+        .map_err(|_| "Invalid input")?;
+
+    let sum: u32 = weights.iter().map(|&w| u32::from(w)).sum();
+    let group_weight = sum / input.part_values(3, 4);
+
+    for subset_size in 1..weights.len() {
+        let mut result = None;
+        visit_subsets(&weights, subset_size, &mut |subset: &[u8]| {
+            if subset.iter().map(|&w| u32::from(w)).sum::<u32>() == group_weight {
+                let product = subset.iter().map(|&w| u128::from(w)).product();
+                if product < result.unwrap_or(u128::MAX) {
+                    result = Some(product);
+                }
+            }
+        });
+        if let Some(product) = result {
+            return Ok(product);
+        }
+    }
+
+    Err("No solution found".to_string())
 }
 
 #[test]
 pub fn tests() {
-    use crate::{test_part_one_error, test_part_two_error};
+    use crate::{test_part_one, test_part_two};
 
     let real_input = include_str!("day24_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
-    test_part_two_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => 10_723_906_903);
+    test_part_two!(real_input => 74_850_409);
 }
--- crates/core/src/year2015/day25.rs
+++ crates/core/src/year2015/day25.rs
@@ -1,13 +1,34 @@
 use crate::Input;
 
-pub fn solve(_input: &mut Input) -> Result<u32, String> {
-    Err("Not yet implemented".to_string())
+pub fn solve(input: &mut Input) -> Result<u64, String> {
+    let words = input.text.split(' ').collect::<Vec<_>>();
+    let wanted_row = words[16][0..(words[16].len() - 1)]
+        .parse::<u32>()
+        .map_err(|_| "Invalid input")?;
+    let wanted_col = words[18][0..(words[18].len() - 1)]
+        .parse::<u32>()
+        .map_err(|_| "Invalid input")?;
+
+    let mut current_code = 20_151_125;
+    let mut current_row = 1;
+    let mut current_col = 1;
+    while (current_row, current_col) != (wanted_row, wanted_col) {
+        if current_row == 1 {
+            current_row = 1 + current_col;
+            current_col = 1;
+        } else {
+            current_col += 1;
+            current_row -= 1;
+        }
+        current_code = (current_code * 252_533) % 33_554_393;
+    }
+    Ok(current_code)
 }
 
 #[test]
 pub fn tests() {
-    use crate::test_part_one_error;
+    use crate::test_part_one;
 
     let real_input = include_str!("day25_input.txt");
-    test_part_one_error!(real_input => "Not yet implemented".to_string());
+    test_part_one!(real_input => 2_650_453);
 }
