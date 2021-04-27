34b706900fbe57050fd265d07a0edd4627119ac1
Fredrik Fornwall <fredrik@fornwall.net>
2021-04-28 00:00:55 +0200

    Speed up 2016-23

--- crates/core/src/year2016/day23.rs
+++ crates/core/src/year2016/day23.rs
@@ -1,15 +1,40 @@
-use super::assembunny::{Computer, Word};
+use super::assembunny::{Computer, Instruction, ValueOrRegister, Word};
 use crate::Input;
+
+fn factorial(num: Word) -> Word {
+    match num {
+        0 | 1 => 1,
+        _ => factorial(num - 1) * num,
+    }
+}
 
 pub fn solve(input: &mut Input) -> Result<Word, String> {
     let mut computer = Computer::parse(input.text)?;
-    computer.registers[0] = input.part_values(7, 12);
+    let register_a_value = input.part_values(7, 12);
+    if computer.instructions.len() > 20 {
+        if let Instruction::Copy(ValueOrRegister::Value(a), _) = computer.instructions[19] {
+            if let Instruction::Jump(ValueOrRegister::Value(b), _) = computer.instructions[20] {
+                return Ok(a * b + factorial(register_a_value));
+            }
+        }
+    }
+
+    computer.registers[0] = register_a_value;
     Ok(computer.execute())
 }
 
 #[test]
 pub fn tests() {
     use crate::{test_part_one, test_part_two};
+
+    let example_input = "cpy 2 a
+tgl a
+tgl a
+tgl a
+cpy 1 a
+dec a
+dec a";
+    test_part_one!(example_input => 3);
 
     let real_input = include_str!("day23_input.txt");
     test_part_one!(real_input => 13_685);
