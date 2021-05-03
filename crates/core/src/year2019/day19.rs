30ae5b93c731d3b58d29faf5a49b807cb8471b80
Fredrik Fornwall <fredrik@fornwall.net>
2021-05-04 00:09:40 +0200

    Rewrite 2019-19 to new format

--- crates/core/src/year2019/day19.rs
+++ crates/core/src/year2019/day19.rs
@@ -1,4 +1,5 @@
 use super::int_code::{Program, Word};
+use crate::input::Input;
 
 fn check(program: &Program, x: i32, y: i32) -> Result<bool, String> {
     let mut program_copy = program.clone();
@@ -13,51 +14,48 @@ fn check(program: &Program, x: i32, y: i32) -> Result<bool, String> {
     Ok(output[0] == 1)
 }
 
-pub fn part1(input_string: &str) -> Result<usize, String> {
-    let program = Program::parse(input_string)?;
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    let program = Program::parse(input.text)?;
 
-    let mut result = 0;
-    for (x, y) in (0..50).flat_map(|x| (0..50).map(move |y| (x, y))) {
-        let checked = check(&program, x, y)?;
-        if checked {
-            result += 1;
+    Ok(if input.is_part_one() {
+        let mut result = 0;
+        for (x, y) in (0..50).flat_map(|x| (0..50).map(move |y| (x, y))) {
+            let checked = check(&program, x, y)?;
+            if checked {
+                result += 1;
+            }
         }
-    }
-    Ok(result)
-}
-
-pub fn part2(input_string: &str) -> Result<i32, String> {
-    let program = Program::parse(input_string)?;
-
-    // Find the initial start of the beam (skipping (0,0),
-    // as it is not connected to rest of the beam.
-    let mut current_x = 0;
-    let mut current_y = 0;
-    for (y, x) in (1..50).flat_map(|x| (0..50).map(move |y| (x, y))) {
-        let checked = check(&program, x, y)?;
-        if checked && current_x == 0 {
-            current_x = x;
-            current_y = y;
+        result
+    } else {
+        // Find the initial start of the beam (skipping (0,0),
+        // as it is not connected to rest of the beam.
+        let mut current_x = 0;
+        let mut current_y = 0;
+        for (y, x) in (1..50).flat_map(|x| (0..50).map(move |y| (x, y))) {
+            let checked = check(&program, x, y)?;
+            if checked && current_x == 0 {
+                current_x = x;
+                current_y = y;
+            }
         }
-    }
 
-    // Track the top right of the beam as long as square does not fit.
-    while !check(&program, current_x - 99, current_y + 99)? {
-        current_y += 1;
-        while check(&program, current_x + 1, current_y)? {
-            current_x += 1;
+        // Track the top right of the beam as long as square does not fit.
+        while !check(&program, current_x - 99, current_y + 99)? {
+            current_y += 1;
+            while check(&program, current_x + 1, current_y)? {
+                current_x += 1;
+            }
         }
-    }
 
-    Ok((current_x - 99) * 10000 + current_y)
-}
-
-#[test]
-pub fn tests_part1() {
-    assert_eq!(part1(include_str!("day19_input.txt")), Ok(112));
+        ((current_x - 99) * 10000 + current_y) as u32
+    })
 }
 
 #[test]
-fn tests_part2() {
-    assert_eq!(part2(include_str!("day19_input.txt")), Ok(18_261_982));
+pub fn tests() {
+    use crate::{test_part_one, test_part_two};
+
+    let input = include_str!("day19_input.txt");
+    test_part_one!(input => 112);
+    test_part_two!(input => 18_261_982);
 }
