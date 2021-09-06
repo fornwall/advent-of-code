b4876f91ad4dc480a39b4b5cf402bb53e0a252da
Fredrik Fornwall <fredrik@fornwall.net>
2021-09-06 20:52:07 +0200

    Switch to async for netlify function

--- crates/wasm/functions/solve.js
+++ crates/wasm/functions/solve.js
@@ -1,43 +1,43 @@
 const solve = require("advent-of-code-rs-wasm").solve;
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context) {
   const headers = {
     "Access-Control-Allow-Origin": "*",
   };
 
   if (event.httpMethod !== "POST") {
-    return callback(null, {
+    return {
       statusCode: 400,
-      body: "This endpoint only supports HTTP POST.",
       headers: {
         Allow: "POST",
       },
-    });
+      body: "This endpoint only supports HTTP POST.",
+    };
   }
 
   const pathParts = event.path.substring(1).split("/");
   if (pathParts.length != 4) {
-    return callback(null, {
+    return {
       statusCode: 400,
       headers,
       body: "Invalid path - expected /solve/{YEAR}/{DAY}/{PART}, was: " + path,
-    });
+    };
   }
   const [, year, day, part] = pathParts;
 
   try {
     const input = event.body;
     const solution = solve(year, day, part, input);
-    return callback(null, {
+    return {
       statusCode: 200,
       headers,
       body: solution,
-    });
+    };
   } catch (e) {
-    return callback(null, {
+    return {
       statusCode: 400,
       headers,
       body: e.message,
-    });
+    };
   }
 };
