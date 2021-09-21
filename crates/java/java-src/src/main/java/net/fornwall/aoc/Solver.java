45b79581c77ac4e8a1c44fae52b9033c5ea33076
Fredrik Fornwall <fredrik@fornwall.net>
2021-09-21 23:57:41 +0200

    Improve javadoc

--- crates/java/java-src/src/main/java/net/fornwall/aoc/Solver.java
+++ crates/java/java-src/src/main/java/net/fornwall/aoc/Solver.java
@@ -2,7 +2,7 @@ package net.fornwall.aoc;
 
 /**
  * A solver of <a href="https://adventofcode.com">Advent of Code</a> problems.
- *
+ * <p>
  * Solutions are implemented in Rust and exposed to Java using JNI - see <a href="https://aoc.fornwall.net">https://aoc.fornwall.net</a>
  */
 public final class Solver {
@@ -11,15 +11,18 @@ public final class Solver {
         JarNativeLibraryLoader.loadLibraryFromJar("advent_of_code_java");
     }
 
+    private Solver() {
+    }
+
     /**
      * Solve the specified problem with the given input.
      *
-     * @param year the year of the problem being solved
-     * @param day the day of the problem being solved (1-25)
-     * @param part the part of the problem being solved (1 or 2)
+     * @param year  the year of the problem being solved
+     * @param day   the day of the problem being solved (1-25)
+     * @param part  the part of the problem being solved (1 or 2)
      * @param input the input text to the problem
-     * @throws SolverException in case of an error happened and the problem could not be solved
      * @return the answer of the specified problem and input
+     * @throws SolverException in case of an error happened and the problem could not be solved
      */
     public static native String solve(int year, int day, int part, String input) throws SolverException;
 
---
+++ crates/java/java-src/src/main/java/net/fornwall/aoc/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains a single {@link net.fornwall.aoc.Solver} class for solving <a href="https://adventofcode.com/">Advent of Code</a> problems.
+ */
+package net.fornwall.aoc;
