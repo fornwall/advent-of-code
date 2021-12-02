329898dc1c3dedd6a17ff2e75fdec2383ff35256
Bump version workflow <bumpversion@users.noreply.github.com>
2021-12-02 20:52:57 +0000

    Bump version: 2019.12.399 → 2019.12.400

--- crates/python/setup.py
+++ crates/python/setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/fornwall/advent-of-code",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2019.12.399",
+    version="2019.12.400",
     rust_extensions=[
         RustExtension(
             "advent_of_code", binding=Binding.PyO3, strip=Strip.All, py_limited_api=True
