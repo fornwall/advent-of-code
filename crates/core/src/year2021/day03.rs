4f87b9b52263b690800c54092a15082279ef0f98
Fredrik Fornwall <fredrik@fornwall.net>
2021-12-04 14:57:20 +0100

    2021, day 3 and 4

--- crates/core/src/year2021/day03.rs
+++ crates/core/src/year2021/day03.rs
@@ -1,17 +1,121 @@
 use crate::input::Input;
 
-pub fn solve(_input: &mut Input) -> Result<String, String> {
-    Ok("".to_string())
+/// Constructs a u16 with the lowest n bits set.
+///
+/// Does not work for n == 0.
+fn set_lowest_bits(n: u8) -> u16 {
+    u16::MAX >> (u16::BITS as u16 - u16::from(n))
+}
+
+fn is_bit_mostly_set(numbers: &[u16], bit_idx: usize) -> bool {
+    numbers.iter().fold(0, |acc, x| {
+        acc + if x & (1 << bit_idx) == 0 { -1 } else { 1 }
+    }) >= 0
+}
+
+fn keep_one_according_to_bitset_criteria(
+    candidates: &mut [u16],
+    num_bits: usize,
+    criteria_wants_most: bool,
+) -> Result<u16, String> {
+    let mut num_candidates = candidates.len();
+    for current_bit_idx in (0..num_bits).rev() {
+        let mostly_set = is_bit_mostly_set(&candidates[0..num_candidates], current_bit_idx);
+        let mut current_candidate_idx = 0;
+        while current_candidate_idx < num_candidates {
+            let is_bit_set = (candidates[current_candidate_idx] & (1 << current_bit_idx)) != 0;
+            if (is_bit_set == mostly_set) == criteria_wants_most {
+                current_candidate_idx += 1;
+            } else {
+                num_candidates -= 1;
+                candidates.swap(current_candidate_idx, num_candidates);
+            }
+        }
+        if num_candidates == 1 {
+            return Ok(candidates[0]);
+        }
+    }
+
+    Err("Bit criteria did not result in single number".to_string())
+}
+
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    const MAX_BITS: usize = u16::BITS as usize;
+
+    let num_bits = input.text.lines().next().map(str::len).unwrap_or_default();
+    if num_bits == 0 || num_bits > MAX_BITS {
+        return Err(format!(
+            "Invalid number of bits - must be between 1 and {}",
+            MAX_BITS
+        ));
+    }
+
+    if input.is_part_one() {
+        let set_bits_at_position = &mut [0; 16][0..num_bits];
+        for line in input.text.lines() {
+            if line.bytes().len() != num_bits {
+                return Err("All lines does not have equal length".to_string());
+            }
+            for (idx, byte) in line.bytes().rev().enumerate() {
+                set_bits_at_position[idx] += if byte == b'1' { 1 } else { -1 };
+            }
+        }
+
+        let gamma: u16 = set_bits_at_position
+            .iter()
+            .enumerate()
+            .filter(|(_bit_idx, &count)| count >= 0)
+            .map(|(bit_idx, _count)| 1 << bit_idx)
+            .sum();
+        let epsilon = !gamma & set_lowest_bits(num_bits as u8);
+        Ok(u32::from(gamma) * u32::from(epsilon))
+    } else {
+        let mut numbers = input
+            .text
+            .lines()
+            .enumerate()
+            .map(|(line_idx, line)| {
+                u16::from_str_radix(line, 2)
+                    .map_err(|_| format!("Line {}: Not a binary integer", line_idx + 1))
+            })
+            .collect::<Result<Vec<u16>, _>>()?;
+
+        let oxygen_generator_rating =
+            keep_one_according_to_bitset_criteria(&mut numbers, num_bits, true)?;
+        let co2_scrubber_rating =
+            keep_one_according_to_bitset_criteria(&mut numbers, num_bits, false)?;
+        Ok(u32::from(oxygen_generator_rating) * u32::from(co2_scrubber_rating))
+    }
+}
+
+#[test]
+fn test_set_bits() {
+    assert_eq!(set_lowest_bits(1), 0b0001);
+    assert_eq!(set_lowest_bits(2), 0b0011);
+    assert_eq!(set_lowest_bits(3), 0b0111);
+    assert_eq!(set_lowest_bits(16), u16::MAX);
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
-    test_part_one!("" => "".to_string());
-    test_part_two!("" => "".to_string());
+    let example = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+    test_part_one!(example => 198);
+    test_part_two!(example => 230);
 
     let real_input = include_str!("day03_input.txt");
-    test_part_one!(real_input => "".to_string());
-    test_part_two!(real_input => "".to_string());
+    test_part_one!(real_input => 3_985_686);
+    test_part_two!(real_input => 2_555_739);
 }
--- crates/core/src/year2021/day04.rs
+++ crates/core/src/year2021/day04.rs
@@ -1,17 +1,126 @@
 use crate::input::Input;
 
-pub fn solve(_input: &mut Input) -> Result<String, String> {
-    Ok("".to_string())
+struct Board {
+    numbers: [u8; 25],
+    drawn_numbers_bitmask: u32,
+}
+
+impl Board {
+    const fn is_bitmask_set(&self, bitmask: u32) -> bool {
+        self.drawn_numbers_bitmask & bitmask == bitmask
+    }
+
+    const fn has_won(&self) -> bool {
+        #![allow(clippy::unusual_byte_groupings)]
+        self.is_bitmask_set(0b00000_00000_00000_00000_11111)
+            || self.is_bitmask_set(0b00000_00000_00000_11111_00000)
+            || self.is_bitmask_set(0b00000_00000_11111_00000_00000)
+            || self.is_bitmask_set(0b00000_11111_00000_00000_00000)
+            || self.is_bitmask_set(0b11111_00000_00000_00000_00000)
+            || self.is_bitmask_set(0b00001_00001_00001_00001_00001)
+            || self.is_bitmask_set(0b00010_00010_00010_00010_00010)
+            || self.is_bitmask_set(0b00100_00100_00100_00100_00100)
+            || self.is_bitmask_set(0b01000_01000_01000_01000_01000)
+            || self.is_bitmask_set(0b10000_10000_10000_10000_10000)
+    }
+
+    fn note_drawn_number(&mut self, drawn_number: u8) -> bool {
+        if let Some(idx) = self.numbers.iter().position(|&e| e == drawn_number) {
+            self.drawn_numbers_bitmask |= 1 << idx;
+            self.has_won()
+        } else {
+            false
+        }
+    }
+
+    fn unmarked_sum(&self) -> u32 {
+        self.numbers
+            .iter()
+            .enumerate()
+            .filter(|(idx, _number)| !self.is_bitmask_set(1 << idx))
+            .map(|(_idx, &number)| u32::from(number))
+            .sum()
+    }
+}
+
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    let mut lines = input.text.lines();
+
+    let drawn_numbers = lines
+        .next()
+        .unwrap_or_default()
+        .split(',')
+        .map(|s| {
+            s.parse::<u8>()
+                .map_err(|_| "Invalid drawn numbers".to_string())
+        })
+        .collect::<Result<Vec<u8>, _>>()?;
+
+    let mut boards = input
+        .text
+        .split("\n\n")
+        .skip(1)
+        .map(|board_str| -> Result<Board, String> {
+            let numbers = board_str
+                .split_ascii_whitespace()
+                .map(|n| {
+                    n.parse::<u8>()
+                        .map_err(|_| "Invalid board - not 25 numbers".to_string())
+                })
+                .collect::<Result<Vec<u8>, String>>()?;
+            Ok(Board {
+                numbers: numbers
+                    .try_into()
+                    .map_err(|_| "Invalid board - not 25 numbers".to_string())?,
+                drawn_numbers_bitmask: 0,
+            })
+        })
+        .collect::<Result<Vec<Board>, _>>()?;
+
+    let wanted_wins = input.part_values(1, boards.len());
+    let mut num_wins = 0;
+
+    for number in drawn_numbers {
+        for board in boards.iter_mut() {
+            if !board.has_won() && board.note_drawn_number(number) {
+                num_wins += 1;
+                if num_wins == wanted_wins {
+                    return Ok(board.unmarked_sum() * u32::from(number));
+                }
+            }
+        }
+    }
+
+    Err("No board won".to_string())
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
-    test_part_one!("" => "".to_string());
-    test_part_two!("" => "".to_string());
+    let example = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+    test_part_one!(example => 4512);
+    test_part_two!(example => 1924);
 
     let real_input = include_str!("day04_input.txt");
-    test_part_one!(real_input => "".to_string());
-    test_part_two!(real_input => "".to_string());
+    test_part_one!(real_input => 8136);
+    test_part_two!(real_input => 12738);
 }
