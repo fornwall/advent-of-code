bbb57d5b4cfee85036ce8d97d47405920a45ce20
Fredrik Fornwall <fredrik@fornwall.net>
2021-12-07 09:53:09 +0100

    2021, day 7

--- crates/core/src/year2021/day07.rs
+++ crates/core/src/year2021/day07.rs
@@ -1,17 +1,47 @@
 use crate::input::Input;
 
-pub fn solve(_input: &mut Input) -> Result<String, String> {
-    Ok("".to_string())
+const fn part_1_fuel_consumption(distance: i32) -> i32 {
+    distance
+}
+
+const fn part_2_fuel_consumption(distance: i32) -> i32 {
+    // Fuel consumption is (with Gauss method):
+    //   1+2+..+distance = distance * (distance + 1) / 2
+    (distance * (distance + 1)) / 2
+}
+
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    let numbers = input
+        .text
+        .split(',')
+        .map(str::parse::<u16>)
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|_| "Input is not comma-separated u16 values".to_string())?;
+
+    let fuel_consumption: fn(i32) -> i32 =
+        input.part_values(part_1_fuel_consumption, part_2_fuel_consumption);
+
+    let max_position = numbers.iter().max().cloned().unwrap_or(0);
+    Ok((0..=max_position)
+        .map(|candidate_spot| {
+            numbers
+                .iter()
+                .map(|n| fuel_consumption((i32::from(*n) - i32::from(candidate_spot)).abs()))
+                .sum::<i32>() as u32
+        })
+        .min()
+        .unwrap_or_default())
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
-    test_part_one!("" => "".to_string());
-    test_part_two!("" => "".to_string());
+    let example = "16,1,2,0,4,2,7,1,2,14";
+    test_part_one!(example => 37);
+    test_part_two!(example => 168);
 
     let real_input = include_str!("day07_input.txt");
-    test_part_one!(real_input => "".to_string());
-    test_part_two!(real_input => "".to_string());
+    test_part_one!(real_input => 328_318);
+    test_part_two!(real_input => 89_791_146);
 }
