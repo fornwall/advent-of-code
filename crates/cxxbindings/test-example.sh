41395aa4eb878b499daa52a8792a254b089a811b
Fredrik Fornwall <fredrik@fornwall.net>
2021-12-10 12:20:26 +0100

    Try to fix CI build of cxxbindings

--- crates/cxxbindings/test-example.sh
+++ crates/cxxbindings/test-example.sh
@@ -7,14 +7,16 @@ mkdir -p target
 cd example/
 
 CXX="c++ -std=c++14 -Wall -Werror"
+LIBS="-ladvent_of_code"
 if [ `uname` = Linux ]; then
-    CXX="$CXX -Wl,-rpath=../../../target/debug/"
+    # CXX="$CXX -Wl,-rpath=../../../target/debug/"
+    LIBS="$LIBS -lm -lpthread -ldl"
 fi
 
 HEADER_PATH=../../../target/cxxbridge/advent-of-code-cxx/src/
 ln -f -s lib.rs.h $HEADER_PATH/advent-of-code.hpp
 
-$CXX -I $HEADER_PATH -L ../../../target/debug/ main.cpp -ladvent_of_code -o ../target/example
+$CXX -I $HEADER_PATH -L ../../../target/debug/ main.cpp $LIBS -o ../target/example
 
 ../target/example > generated-output.txt
 diff -u expected-output.txt generated-output.txt
