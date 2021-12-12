2eb4e75aaf1f2d3a5152ec7d9dc08dc956c7ba41
Fredrik Fornwall <fredrik@fornwall.net>
2021-12-12 19:12:59 +0100

    Speed up 2021-12

--- crates/core/src/year2021/day12.rs
+++ crates/core/src/year2021/day12.rs
@@ -15,6 +15,23 @@ pub fn solve(input: &mut Input) -> Result<u64, String> {
     ))
 }
 
+struct IdAssigner<'a> {
+    id_map: HashMap<&'a str, u8>,
+}
+
+impl<'a> IdAssigner<'a> {
+    fn new() -> Self {
+        Self {
+            id_map: HashMap::new(),
+        }
+    }
+
+    fn id_of(&mut self, name: &'a str) -> u8 {
+        let next_id = self.id_map.len() as u8;
+        *self.id_map.entry(name).or_insert(next_id)
+    }
+}
+
 struct CaveMap {
     connections: [u16; CaveMap::MAX_SIZE],
     big_caves: u16,
@@ -25,12 +42,7 @@ struct CaveMap {
 impl CaveMap {
     const MAX_SIZE: usize = 16;
 
-    const fn path_from(&self, from: u8, to: u8) -> bool {
-        (self.connections[from as usize] & (1 << to)) > 0
-    }
-
     fn parse(text: &str) -> Result<Self, String> {
-        let mut current_id = 0_u8;
         let mut result = Self {
             connections: [0; Self::MAX_SIZE],
             big_caves: 0,
@@ -38,17 +50,13 @@ impl CaveMap {
             end: u8::MAX,
         };
 
-        let mut map = HashMap::new();
+        let mut id_assigner = IdAssigner::new();
+
         for line in text.lines() {
             if let Some((from, to)) = line.split_once('-') {
-                let from_id = *map.entry(from).or_insert_with(|| {
-                    current_id += 1;
-                    current_id - 1
-                });
-                let to_id = *map.entry(to).or_insert_with(|| {
-                    current_id += 1;
-                    current_id - 1
-                });
+                let from_id = id_assigner.id_of(from);
+                let to_id = id_assigner.id_of(to);
+
                 if to_id >= Self::MAX_SIZE as u8 {
                     return Err(format!(
                         "Too many distinct nodes - only {} supported",
@@ -61,7 +69,9 @@ impl CaveMap {
 
                 if from == "start" {
                     result.start = from_id;
-                } else if to == "end" {
+                };
+
+                if to == "end" {
                     result.end = to_id;
                 };
 
@@ -92,35 +102,36 @@ impl CaveMap {
         }
 
         let mut result = 0;
-        for i in 0..16 {
-            let i_bitmask = 1 << i;
-            let has_visited_once = visited_once & i_bitmask > 0;
-            let is_big_cave = self.big_caves & i_bitmask > 0;
-            let i_is_cave_start_or_end = i == self.start || i == self.end;
-            if self.path_from(start, i)
-                && matches!(
-                    (
-                        has_visited_once,
-                        visited_small_twice,
-                        is_big_cave,
-                        i_is_cave_start_or_end,
-                        part2
-                    ),
-                    // Can always visit a big cave:
-                    (_, _, true, _, _)
+        let mut connections_from_start = self.connections[start as usize];
+        while connections_from_start != 0 {
+            let destination_id = connections_from_start.trailing_zeros() as u8;
+            let destination_bitmask = 1 << destination_id;
+            connections_from_start ^= destination_bitmask;
+            let has_visited_once = visited_once & destination_bitmask > 0;
+            let is_big_cave = self.big_caves & destination_bitmask > 0;
+            let is_start_or_end = destination_id == self.start || destination_id == self.end;
+            if matches!(
+                (
+                    has_visited_once,
+                    visited_small_twice,
+                    is_big_cave,
+                    is_start_or_end,
+                    part2
+                ),
+                // Can always visit a big cave:
+                (_, _, true, _, _)
                 // Can always visit an unvisited cave:
                 | (false, _, _, _, _)
                 // In part 2, can always visit a cave we haven't
                 // visited twice (unless it is the cave start or end):
                 | (_, false, _, false, true)
-                )
-            {
+            ) {
                 let now_visited_small_twice =
                     visited_small_twice || (has_visited_once && !is_big_cave);
                 result += self.search(
-                    visited_once | i_bitmask,
+                    visited_once | destination_bitmask,
                     now_visited_small_twice,
-                    i,
+                    destination_id,
                     end,
                     part2,
                 );
