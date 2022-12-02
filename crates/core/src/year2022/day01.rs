7f7c1963320c7d51013a209c43ea0142cc8b8194
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-02 07:20:04 +0100

    2022 has started

--- crates/core/src/year2022/day01.rs
+++ crates/core/src/year2022/day01.rs
@@ -1,14 +1,78 @@
 use crate::input::Input;
 
-pub fn solve(_input: &mut Input) -> Result<i32, String> {
-    Ok(0)
+/// The highest `NUM` values out of the values provided to `on_value(new_value)`.
+struct HighestValues<const NUM: usize> {
+    values: [u64; NUM],
+}
+
+impl<const NUM: usize> HighestValues<NUM> {
+    const fn new() -> Self {
+        Self { values: [0; NUM] }
+    }
+
+    fn on_value(&mut self, new_value: u64) {
+        self.values.sort_unstable();
+        for existing_value in self.values.iter_mut() {
+            if *existing_value < new_value {
+                *existing_value = new_value;
+                return;
+            }
+        }
+    }
+
+    fn sum(&self) -> u64 {
+        self.values.iter().sum()
+    }
+}
+
+impl<const NUM: usize> FromIterator<u64> for HighestValues<NUM> {
+    fn from_iter<I: IntoIterator<Item = u64>>(iter: I) -> Self {
+        let mut highest = Self::new();
+        for i in iter {
+            highest.on_value(i);
+        }
+        highest
+    }
+}
+
+pub fn solve(input: &mut Input) -> Result<u64, String> {
+    if input.is_part_one() {
+        solve_part::<1>(input)
+    } else {
+        solve_part::<3>(input)
+    }
+}
+
+pub fn solve_part<const NUM: usize>(input: &mut Input) -> Result<u64, String> {
+    Ok(input
+        .text
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|line| u64::from(line.parse::<u32>().unwrap_or_default()))
+                .sum()
+        })
+        .collect::<HighestValues<NUM>>()
+        .sum())
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
+    let test_input = "1000\n\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+    test_part_one!(test_input => 24_000);
+    test_part_two!(test_input => 45_000);
+
+    let test_input = "4294967296";
+    test_part_one!(test_input => 0);
+    test_part_two!(test_input => 0);
+
+    let test_input = "4294967295\n\n1\n\n1";
+    test_part_one!(test_input => 4_294_967_295);
+    test_part_two!(test_input => 4_294_967_297);
+
     let real_input = include_str!("day01_input.txt");
-    test_part_one!(real_input => 0);
-    test_part_two!(real_input => 0);
+    test_part_one!(real_input => 71_300);
+    test_part_two!(real_input => 209_691);
 }
--- crates/core/src/year2022/day02.rs
+++ crates/core/src/year2022/day02.rs
@@ -1,14 +1,43 @@
 use crate::input::Input;
 
-pub fn solve(_input: &mut Input) -> Result<i32, String> {
-    Ok(0)
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    input
+        .text
+        .lines()
+        .map(|line| {
+            let line = line.as_bytes();
+            if !(line.len() == 3
+                && (b'A'..=b'C').contains(&line[0])
+                && (b'X'..=b'Z').contains(&line[2]))
+            {
+                return Err("Invalid input".to_string());
+            }
+
+            let other_shape = (line[0] - b'A') as i8;
+            let second_value = (line[2] - b'X') as i8;
+
+            let my_shape =
+                input.part_values(second_value, (other_shape - 1 + second_value).rem_euclid(3));
+
+            let my_shape_score = 1 + my_shape as u32;
+            let my_outcome_score = match (other_shape - my_shape).rem_euclid(3) {
+                2 => 6,
+                1 => 0,
+                _ => 3_u32,
+            };
+            Ok(my_shape_score + my_outcome_score)
+        })
+        .sum()
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
+    let test_input = "A Y\nB X\nC Z";
+    test_part_one!(test_input => 15);
+
     let real_input = include_str!("day02_input.txt");
-    test_part_one!(real_input => 0);
-    test_part_two!(real_input => 0);
+    test_part_one!(real_input => 11063);
+    test_part_two!(real_input => 10349);
 }
