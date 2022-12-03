24bca35ca23b20db86cb6e9a46aac4eef13d77cd
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-03 06:49:32 +0100

    2022-03

--- crates/core/src/year2022/day03.rs
+++ crates/core/src/year2022/day03.rs
@@ -1,14 +1,63 @@
 use crate::input::Input;
+use std::collections::HashSet;
 
-pub fn solve(_input: &mut Input) -> Result<i32, String> {
-    Ok(0)
+pub fn solve(input: &mut Input) -> Result<u32, String> {
+    Ok(if input.is_part_one() {
+        input
+            .text
+            .lines()
+            .map(|line| {
+                HashSet::<u8>::from_iter((line[0..line.len() / 2]).bytes())
+                    .intersection(&HashSet::from_iter((line[line.len() / 2..]).bytes()))
+                    .copied()
+                    .map(item_priority)
+                    .sum::<u32>()
+            })
+            .sum()
+    } else {
+        input
+            .text
+            .lines()
+            .collect::<Vec<_>>()
+            .chunks(3)
+            .map(|group| {
+                HashSet::<u8>::from_iter(group[0].bytes())
+                    .intersection(&HashSet::from_iter(group[1].bytes()))
+                    .copied()
+                    .collect::<HashSet<_>>()
+                    .intersection(&HashSet::from_iter(group[2].bytes()))
+                    .copied()
+                    .map(item_priority)
+                    .sum::<u32>()
+            })
+            .sum()
+    })
+}
+
+fn item_priority(b: u8) -> u32 {
+    u32::from(if b.is_ascii_lowercase() {
+        b - b'a' + 1
+    } else if b.is_ascii_uppercase() {
+        b - b'A' + 27
+    } else {
+        0
+    })
 }
 
 #[test]
 pub fn tests() {
     use crate::input::{test_part_one, test_part_two};
 
+    let test_input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+    test_part_one!(test_input => 157);
+    test_part_two!(test_input => 70);
+
     let real_input = include_str!("day03_input.txt");
-    test_part_one!(real_input => 0);
-    test_part_two!(real_input => 0);
+    test_part_one!(real_input => 8176);
+    test_part_two!(real_input => 2689);
 }
