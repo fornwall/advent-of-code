4d6016c895ce725763d0c18d7f5610774ffc7dba
Fredrik Fornwall <fredrik@fornwall.net>
2023-01-29 00:19:57 +0100

    Remove newline mess

--- crates/core/src/year2022/day24_renderer.rs
+++ crates/core/src/year2022/day24_renderer.rs
@@ -136,48 +136,48 @@ impl Renderer {
         let reachable_odd_path_id = self.svg.add_with_id(SvgGroup::new().class("elf"));
         self.svg.add(SvgScript::new(format!(
             "{};\n\
-                        const leftBlizzards = document.querySelectorAll('.blizzard-left');\n\
-                        const rightBlizzards = document.querySelectorAll('.blizzard-right');\n\
-                        const upBlizzards = document.querySelectorAll('.blizzard-up');\n\
-                        const downBlizzards = document.querySelectorAll('.blizzard-down');\n\
-                        const evenPath = document.getElementById('{}');\n\
-                        const oddPath = document.getElementById('{}');\n\
-                        const width = {};\n\
-                        const height = {};\n\
-                        const mod = (n, m) => (n % m + m) % m;\n\
-                        window.onNewStep = (step) => {{\n\
-                            const newCircles = reachablePerStep[step].map(a => {{\n\
-                                const c = document.createElementNS('http://www.w3.org/2000/svg', 'use');\n\
-                                c.setAttribute('href', '#{}');\n\
-                                c.setAttribute('x', a[0]);\n\
-                                c.setAttribute('y', a[1]);\n\
-                                return c;\n
+                        const leftBlizzards = document.querySelectorAll('.blizzard-left');
+                        const rightBlizzards = document.querySelectorAll('.blizzard-right');
+                        const upBlizzards = document.querySelectorAll('.blizzard-up');
+                        const downBlizzards = document.querySelectorAll('.blizzard-down');
+                        const evenPath = document.getElementById('{}');
+                        const oddPath = document.getElementById('{}');
+                        const width = {};
+                        const height = {};
+                        const mod = (n, m) => (n % m + m) % m;
+                        window.onNewStep = (step) => {{
+                            const newCircles = reachablePerStep[step].map(a => {{
+                                const c = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+                                c.setAttribute('href', '#{}');
+                                c.setAttribute('x', a[0]);
+                                c.setAttribute('y', a[1]);
+                                return c;
                             }});\n\
-                            const [oldPath, newPath] = (step % 2 == 0) ? [oddPath, evenPath] : [evenPath, oddPath];\n\
-                            oldPath.setAttribute('fill-opacity', 0);\n\
-                            newPath.setAttribute('fill-opacity', 1);\n\
-                            newPath.replaceChildren(...newCircles);\n\
-                            for (let [idx, el] of leftBlizzards.entries()) {{\n\
-                                let amount = mod((idx - step), width);\n\
-                                if (amount === width -1) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}\n\
-                                el.style.transform = `translate(${{amount}}px,0px)`;\n\
+                            const [oldPath, newPath] = (step % 2 == 0) ? [oddPath, evenPath] : [evenPath, oddPath];
+                            oldPath.setAttribute('fill-opacity', 0);
+                            newPath.setAttribute('fill-opacity', 1);
+                            newPath.replaceChildren(...newCircles);
+                            for (let [idx, el] of leftBlizzards.entries()) {{
+                                let amount = mod((idx - step), width);
+                                if (amount === width -1) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}
+                                el.style.transform = `translate(${{amount}}px,0px)`;
+                            }}
+                            for (let [idx, el] of rightBlizzards.entries()) {{
+                                let amount = mod((idx + step), width);
+                                if (amount === 0) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}
+                                el.style.transform = `translate(${{amount}}px,0px)`;
                             }}\n\
-                            for (let [idx, el] of rightBlizzards.entries()) {{\n\
-                                let amount = mod((idx + step), width);\n\
-                                if (amount === 0) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}\n\
-                                el.style.transform = `translate(${{amount}}px,0px)`;\n\
-                            }}\n\
-                            for (let [idx, el] of upBlizzards.entries()) {{\n\
-                                let amount = mod((idx - step), height);\n\
-                                if (amount === height - 1) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}\n\
-                                el.style.transform = `translate(0px,${{amount}}px)`;\n\
-                            }}\n\
-                            for (let [idx, el] of downBlizzards.entries()) {{\n\
-                                let amount = mod((idx + step), height);\n\
-                                if (amount === 0) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}\n\
-                                el.style.transform = `translate(0px,${{amount}}px)`;\n\
-                            }}\n\
-                        }};\n",
+                            for (let [idx, el] of upBlizzards.entries()) {{
+                                let amount = mod((idx - step), height);
+                                if (amount === height - 1) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}
+                                el.style.transform = `translate(0px,${{amount}}px)`;
+                            }}
+                            for (let [idx, el] of downBlizzards.entries()) {{
+                                let amount = mod((idx + step), height);
+                                if (amount === 0) {{ el.style.transition = 'none'; }} else {{ el.style.transition = ''; }}
+                                el.style.transform = `translate(0px,${{amount}}px)`;
+                            }}
+                        }};",
             reachable_array_js, reachable_even_path_id, reachable_odd_path_id, valley.width, valley.height, reachable_circle_id
         )));
         self.svg.add(
