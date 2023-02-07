6c65cb6fcec90fc7fbacafd919746e0e3a594b6e
Fredrik Fornwall <fredrik@fornwall.net>
2023-02-07 21:56:35 +0100

    Add comments to simd

--- crates/core/src/year2022/day23_simd.rs
+++ crates/core/src/year2022/day23_simd.rs
@@ -3,26 +3,17 @@
 /// Taken from https://github.com/Crazytieguy/advent-of-code/blob/master/2022/src/bin/day23/main.rs
 use std::array;
 use std::collections::VecDeque;
-use std::ops::Range;
 use std::simd::u8x32;
-use crate::chain;
 
+use crate::chain;
 use crate::input::Input;
 
 pub fn solve(input: &Input) -> Result<usize, String> {
-    let mut grid = ElfGrid::new();
-    input.text.lines().enumerate().for_each(|(row, line)| {
-        for (col, b) in line.bytes().enumerate() {
-            if b == b'#' {
-                grid.set_elf_at(row + ELF_GRID_ROW_OFFSET, col + ELF_GRID_COL_OFFSET);
-            }
-        }
-    });
+    let mut grid = ElfGrid::parse(input.text)?;
 
     if input.is_part_one() {
         grid.run_simulation(10);
-        let (rows, cols) = grid.bounds();
-        Ok(rows.len() * cols.len() - grid.num_elves())
+        Ok(grid.populated_rect_size() - grid.num_elves())
     } else {
         grid.run_simulation(10000)
             .ok_or_else(|| "No solution found in 10,000 rounds".to_string())
@@ -30,11 +21,20 @@ pub fn solve(input: &Input) -> Result<usize, String> {
 }
 
 type ElfGridRow = u8x32;
+
 const ELF_GRID_NUM_ROWS: usize = 160;
+const ELF_GRID_NUM_COLS: usize = 256;
 const ELF_GRID_ROW_OFFSET: usize = 24;
 const ELF_GRID_COL_OFFSET: usize = 72;
 
 struct ElfGrid {
+    /// Each row is represented as a bitset.
+    /// - Bit 0 in lane 0 is for col 0
+    /// - [...]
+    /// - Bit 1 in lane 0 is for col 1
+    /// - Bit 7 in lane 0 is for col 7
+    /// - Bit 0 in lane 1 is for col 8
+    /// - [...]
     bit_rows: [ElfGridRow; ELF_GRID_NUM_ROWS],
 }
 
@@ -47,10 +47,23 @@ enum Direction {
 }
 
 impl ElfGrid {
-    fn new() -> Self {
-        Self {
+    fn parse(input: &str) -> Result<Self, String> {
+        let mut grid = Self {
             bit_rows: [ElfGridRow::splat(0); ELF_GRID_NUM_ROWS],
+        };
+        for (row, line) in input.lines().enumerate() {
+            for (col, b) in line.bytes().enumerate() {
+                if b == b'#' {
+                    let storage_row = row + ELF_GRID_ROW_OFFSET;
+                    let storage_col = col + ELF_GRID_COL_OFFSET;
+                    if storage_row >= ELF_GRID_NUM_ROWS || storage_col >= ELF_GRID_NUM_COLS {
+                        return Err("Elves does not fit into optimised grid".to_string());
+                    }
+                    grid.set_elf_at(storage_row, storage_col);
+                }
+            }
         }
+        Ok(grid)
     }
 
     fn set_elf_at(&mut self, row: usize, col: usize) {
@@ -61,12 +74,24 @@ impl ElfGrid {
         self.bit_rows[row][col / 8] & (1 << (col % 8)) != 0
     }
 
-    fn shift_west(&row: &ElfGridRow) -> ElfGridRow {
-        (row >> ElfGridRow::splat(1)) | (row.rotate_lanes_left::<1>() << ElfGridRow::splat(7))
+    fn shift_cols_west(&row: &ElfGridRow) -> ElfGridRow {
+        // Shift cols to the west/left (to _lower_ values).
+        // Start with each lane shifted right (so lowest bit is lost):
+        // [abcd, efgh, ijkl] -> [0abc, 0efg, 0ijk]
+        (row >> ElfGridRow::splat(1))
+            // Bitwise OR with the lowest bit shifted to highest, with rotated lanes.
+            // [abcd, efgh, ijkl] -> [h000, l000, d000]
+            | (row.rotate_lanes_left::<1>() << ElfGridRow::splat(7))
     }
 
-    fn shift_east(&row: &ElfGridRow) -> ElfGridRow {
-        (row << ElfGridRow::splat(1)) | (row.rotate_lanes_right::<1>() >> ElfGridRow::splat(7))
+    fn shift_cols_east(&row: &ElfGridRow) -> ElfGridRow {
+        // Shift cols to the east/right (to _higher_ values).
+        // Start with each lane shifted left (so highest bit is lost):
+        // [abcd, efgh, ijkl] -> [bcd0, fgh0, jkl0]
+        (row << ElfGridRow::splat(1))
+            // Bitwise OR with the highest bit shifted to lowest, with rotated lanes.
+            // [abcd, efgh, ijkl] -> [000l, 000a, 000e]
+            | (row.rotate_lanes_right::<1>() >> ElfGridRow::splat(7))
     }
 
     fn run_simulation(&mut self, max_rounds: usize) -> Option<usize> {
@@ -85,28 +110,54 @@ impl ElfGrid {
         None
     }
 
-    fn play_round(&mut self, priority: [Direction; 4]) -> bool {
-        fn propose(
+    fn play_round(&mut self, ordered_directions: [Direction; 4]) -> bool {
+        // Given 9 rows:
+        // - The above row:   [shifted-east, unmodified, shifted-west]
+        // - The current row: [shifted-east, unmodified, shifted-west]
+        // - The below row:   [shifted-east, unmodified, shifted-west]
+        //
+        // This function computes an array, indexed by direction (N=1, S=2, W=3, E=4)
+        // mapping to a bitset of those elves who will be moving in the specified direction
+        // (not taking collisions into account).
+        //
+        // Note that e.g. "the above row, shifted-east" is actually "nw" in the function signature.
+        // That is since it contains the bits shifted east, so shows the population to the west.
+        fn bitset_per_direction_excluding_collisions(
             [nw, n, ne]: &[ElfGridRow; 3],
             [w, cur, e]: &[ElfGridRow; 3],
             [sw, s, se]: &[ElfGridRow; 3],
-            directions: [Direction; 4],
+            ordered_directions: [Direction; 4],
         ) -> [ElfGridRow; 4] {
             let mut propositions = [*cur; 4];
+            // Setup available (unclaimed bit sets):
             let mut not_chosen = nw | n | ne | w | e | sw | s | se;
-            for d in directions {
-                let (row, dir_available) = match d {
-                    Direction::North => (&mut propositions[0], !(ne | n | nw)),
-                    Direction::South => (&mut propositions[1], !(se | s | sw)),
-                    Direction::West => (&mut propositions[2], !(nw | w | sw)),
-                    Direction::East => (&mut propositions[3], !(ne | e | se)),
+            for d in ordered_directions {
+                let (row, direction_occupied) = match d {
+                    Direction::North => (&mut propositions[0], (ne | n | nw)),
+                    Direction::South => (&mut propositions[1], (se | s | sw)),
+                    Direction::West => (&mut propositions[2], (nw | w | sw)),
+                    Direction::East => (&mut propositions[3], (ne | e | se)),
                 };
-                *row &= dir_available & not_chosen;
-                not_chosen &= !dir_available;
+                // Starting from current bit, only keep if
+                // - All three relevant directions are unoccupied
+                // -
+                *row &= !direction_occupied & not_chosen;
+                // Only keep not bits if the direction was occupied:
+                not_chosen &= direction_occupied;
             }
             propositions
         }
 
+        // Given 3 results from bitset_per_direction_excluding_collisions() above
+        // - How the above row would move [N, S, W, E]
+        // - How the current row would move [N, S, W, E]
+        // - How the below row would move [N, S, W, E]
+        // if there were no collisions, this function computes how the current row
+        // will be populated by elves moving from the north, south, weast and east.
+        //
+        // Output format is an array, indexed by direction (N=1, S=2, W=3, E=4)
+        // mapping to a bitset of those elves who will be coming from the specified
+        // direction - now taking collisions into account.
         fn collide_proposals(
             [_, south, _, _]: &[ElfGridRow; 4],
             [_, _, west, east]: &[ElfGridRow; 4],
@@ -115,29 +166,29 @@ impl ElfGrid {
             [
                 north & !*south,
                 south & !*north,
-                ElfGrid::shift_west(west) & !ElfGrid::shift_east(east),
-                ElfGrid::shift_east(east) & !ElfGrid::shift_west(west),
+                ElfGrid::shift_cols_west(west) & !ElfGrid::shift_cols_east(east),
+                ElfGrid::shift_cols_east(east) & !ElfGrid::shift_cols_west(west),
             ]
         }
 
         let mut new_bit_rows = self.bit_rows;
         let mut moved = false;
-        let empty_row = [ElfGridRow::splat(0); 2];
+        let two_empty_rows = [ElfGridRow::splat(0); 2];
 
-        chain!(&empty_row, &self.bit_rows, &empty_row)
-            .map(|row| [Self::shift_east(row), *row, Self::shift_west(row)])
-            .map_windows(|[above, cur, below]| propose(above, cur, below, priority))
+        chain!(&two_empty_rows, &self.bit_rows, &two_empty_rows)
+            .map(|row| [Self::shift_cols_east(row), *row, Self::shift_cols_west(row)])
+            .map_windows(|[above, cur, below]| bitset_per_direction_excluding_collisions(above, cur, below, ordered_directions))
             .map_windows(|[above, cur, below]| collide_proposals(above, cur, below))
             .enumerate()
-            .for_each(|(i, [from_south, from_north, from_east, from_west])| {
+            .for_each(|(row_idx, [from_south, from_north, from_east, from_west])| {
                 let destinations = from_north | from_south | from_west | from_east;
                 if destinations != ElfGridRow::splat(0) {
                     moved = true;
-                    new_bit_rows[i + 1] &= !from_south;
-                    new_bit_rows[i - 1] &= !from_north;
-                    new_bit_rows[i] &= !Self::shift_west(&from_west);
-                    new_bit_rows[i] &= !Self::shift_east(&from_east);
-                    new_bit_rows[i] |= destinations;
+                    new_bit_rows[row_idx + 1] &= !from_south;
+                    new_bit_rows[row_idx - 1] &= !from_north;
+                    new_bit_rows[row_idx] &= !Self::shift_cols_west(&from_west);
+                    new_bit_rows[row_idx] &= !Self::shift_cols_east(&from_east);
+                    new_bit_rows[row_idx] |= destinations;
                 }
             });
 
@@ -145,22 +196,17 @@ impl ElfGrid {
         moved
     }
 
-    fn bounds(&self) -> (Range<usize>, Range<usize>) {
-        let mut min_row = usize::MAX;
-        let mut max_row = usize::MIN;
-        let mut min_col = usize::MAX;
-        let mut max_col = usize::MIN;
-        for row in 0..self.bit_rows.len() {
-            for col in 0..256 {
-                if self.is_elf_at(row, col) {
-                    min_row = min_row.min(row);
-                    max_row = max_row.max(row);
-                    min_col = min_col.min(col);
-                    max_col = max_col.max(col);
-                }
+    fn populated_rect_size(&self) -> usize {
+        let bounds = (0..self.bit_rows.len()).flat_map(|row|
+            (0..ELF_GRID_NUM_COLS).map(move |col| (row, col))
+        ).fold((usize::MAX, usize::MIN, usize::MAX, usize::MIN), |acc, (row, col)| {
+            if self.is_elf_at(row, col) {
+                (acc.0.min(row), acc.1.max(row), acc.2.min(col), acc.3.max(col))
+            } else {
+                acc
             }
-        }
-        (min_row..max_row + 1, min_col..max_col + 1)
+        });
+        (bounds.1 + 1 - bounds.0) * (bounds.3 + 1 - bounds.2)
     }
 
     fn num_elves(&self) -> usize {
@@ -186,7 +232,7 @@ impl<I: Iterator, F, T, const N: usize> MapWindows<I, F, T, N>
         F: FnMut([&I::Item; N]) -> T,
 {
     fn new(mut iter: I, f: F) -> Self {
-        let buf = iter.by_ref().take(N - 1).collect();
+        let buf: VecDeque<_> = iter.by_ref().take(N - 1).collect();
         Self { iter, f, buf }
     }
 }
@@ -218,3 +264,14 @@ trait MapWindowsIterator: Iterator {
 }
 
 impl<I: Iterator> MapWindowsIterator for I {}
+
+#[test]
+fn test_iterator() {
+    let v = [1, 2, 3, 4].iter().map_windows(|[a, b]| (**a, **b)).collect::<Vec<_>>();
+    assert_eq!(vec![(1, 2), (2, 3), (3, 4)], v);
+
+    let v = [1, 2, 3, 4].iter().map_windows(|[a, b, c]| (**a, **b, **c)).collect::<Vec<_>>();
+    assert_eq!(vec![(1, 2, 3), (2, 3, 4)], v);
+    let v = [1, 2, 3, 4].iter().map_windows(|[a, b, c, d, e]| (**a, **b, **c, **d, **e)).next();
+    assert_eq!(None, v);
+}
