2c38261724b75f940219500eade3d0533e6586c0
Fredrik Fornwall <fredrik@fornwall.net>
2023-11-16 18:18:13 +0100

    Fix clippy warnings

--- crates/core/src/common/u256.rs
+++ crates/core/src/common/u256.rs
@@ -15,16 +15,16 @@ impl U256 {
         }
     }
 
-    pub fn count_ones(&self) -> u32 {
+    pub const fn count_ones(&self) -> u32 {
         self.left.count_ones() + self.right.count_ones()
     }
 
-    pub fn non_zero(&self) -> bool {
+    pub const fn non_zero(&self) -> bool {
         self.left != 0 || self.right != 0
     }
 
     /// Used to find the bounding rectangle for part one.
-    pub fn min_set(&self) -> Option<u32> {
+    pub const fn min_set(&self) -> Option<u32> {
         if self.left != 0 {
             Some(self.left.leading_zeros())
         } else if self.right != 0 {
@@ -35,7 +35,7 @@ impl U256 {
     }
 
     /// Used to find the bounding rectangle for part one.
-    pub fn max_set(&self) -> Option<u32> {
+    pub const fn max_set(&self) -> Option<u32> {
         if self.right != 0 {
             Some(255 - self.right.trailing_zeros())
         } else if self.left != 0 {
@@ -45,15 +45,15 @@ impl U256 {
         }
     }
 
-    pub fn left_shift(&self) -> U256 {
-        U256 {
+    pub const fn left_shift(&self) -> Self {
+        Self {
             left: (self.left << 1) | (self.right >> 127),
             right: (self.right << 1),
         }
     }
 
-    pub fn right_shift(&self) -> U256 {
-        U256 {
+    pub const fn right_shift(&self) -> Self {
+        Self {
             left: (self.left >> 1),
             right: (self.left << 127) | (self.right >> 1),
         }
@@ -62,10 +62,10 @@ impl U256 {
 
 /// Syntactic sugar to provide the regular `&`, `|` and `!` bitwise operator notation.
 impl BitAnd for U256 {
-    type Output = U256;
+    type Output = Self;
 
-    fn bitand(self, rhs: U256) -> U256 {
-        U256 {
+    fn bitand(self, rhs: Self) -> Self {
+        Self {
             left: self.left & rhs.left,
             right: self.right & rhs.right,
         }
@@ -73,10 +73,10 @@ impl BitAnd for U256 {
 }
 
 impl BitOr for U256 {
-    type Output = U256;
+    type Output = Self;
 
-    fn bitor(self, rhs: U256) -> U256 {
-        U256 {
+    fn bitor(self, rhs: Self) -> Self {
+        Self {
             left: self.left | rhs.left,
             right: self.right | rhs.right,
         }
@@ -84,10 +84,10 @@ impl BitOr for U256 {
 }
 
 impl Not for U256 {
-    type Output = U256;
+    type Output = Self;
 
-    fn not(self) -> U256 {
-        U256 {
+    fn not(self) -> Self {
+        Self {
             left: !self.left,
             right: !self.right,
         }
@@ -95,7 +95,7 @@ impl Not for U256 {
 }
 
 impl BitAndAssign for U256 {
-    fn bitand_assign(&mut self, rhs: U256) {
+    fn bitand_assign(&mut self, rhs: Self) {
         self.left &= rhs.left;
         self.right &= rhs.right;
     }
