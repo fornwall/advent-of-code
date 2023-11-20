1d60991308036471b4bed48c797262d7589efa36
Fredrik Fornwall <fredrik@fornwall.net>
2023-11-21 00:55:12 +0100

    2021-25: Speed up

--- crates/core/src/common/u256.rs
+++ crates/core/src/common/u256.rs
@@ -1,73 +1,76 @@
-use std::ops::{BitAnd, BitAndAssign, BitOr, Not};
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not};
 
 #[derive(Clone, Copy, Default)]
 pub struct U256 {
-    left: u128,
-    right: u128,
+    pub low: u128,
+    pub high: u128,
 }
 
 impl U256 {
-    pub fn bit_set(&mut self, offset: usize) {
+    pub fn set_bit(&mut self, offset: usize) {
         if offset < 128 {
-            self.left |= 1 << (127 - offset);
+            self.low |= 1 << offset;
         } else {
-            self.right |= 1 << (255 - offset);
+            self.high |= 1 << (offset - 128);
+        }
+    }
+
+    pub const fn non_zero(&self) -> bool {
+        self.low != 0 || self.high != 0
+    }
+
+    /*
+    pub fn is_bit_set(&self, offset: usize) -> bool {
+        if offset < 128 {
+            (self.low & 1 << offset) != 0
+        } else {
+            (self.high & 1 << (offset - 128)) != 0
         }
     }
 
     pub const fn count_ones(&self) -> u32 {
-        self.left.count_ones() + self.right.count_ones()
+        self.low.count_ones() + self.high.count_ones()
     }
 
-    pub const fn non_zero(&self) -> bool {
-        self.left != 0 || self.right != 0
-    }
-
-    /// Used to find the bounding rectangle for part one.
-    pub const fn min_set(&self) -> Option<u32> {
-        if self.left != 0 {
-            Some(self.left.leading_zeros())
-        } else if self.right != 0 {
-            Some(128 + self.right.leading_zeros())
+    pub const fn leading_zeros(&self) -> u32 {
+        if self.high != 0 {
+            self.high.leading_zeros()
         } else {
-            None
+            128 + self.low.leading_zeros()
         }
     }
 
-    /// Used to find the bounding rectangle for part one.
-    pub const fn max_set(&self) -> Option<u32> {
-        if self.right != 0 {
-            Some(255 - self.right.trailing_zeros())
-        } else if self.left != 0 {
-            Some(127 - self.left.trailing_zeros())
+    pub const fn trailing_zeros(&self) -> u32 {
+        if self.low != 0 {
+            self.low.trailing_zeros()
         } else {
-            None
+            self.high.trailing_zeros() + 128
         }
     }
 
     pub const fn left_shift(&self) -> Self {
         Self {
-            left: (self.left << 1) | (self.right >> 127),
-            right: (self.right << 1),
+            low: (self.low << 1) | (self.high >> 127),
+            high: (self.high << 1),
         }
     }
 
     pub const fn right_shift(&self) -> Self {
         Self {
-            left: (self.left >> 1),
-            right: (self.left << 127) | (self.right >> 1),
+            low: (self.low >> 1),
+            high: (self.low << 127) | (self.high >> 1),
         }
     }
+     */
 }
 
-/// Syntactic sugar to provide the regular `&`, `|` and `!` bitwise operator notation.
 impl BitAnd for U256 {
     type Output = Self;
 
     fn bitand(self, rhs: Self) -> Self {
         Self {
-            left: self.left & rhs.left,
-            right: self.right & rhs.right,
+            low: self.low & rhs.low,
+            high: self.high & rhs.high,
         }
     }
 }
@@ -77,8 +80,8 @@ impl BitOr for U256 {
 
     fn bitor(self, rhs: Self) -> Self {
         Self {
-            left: self.left | rhs.left,
-            right: self.right | rhs.right,
+            low: self.low | rhs.low,
+            high: self.high | rhs.high,
         }
     }
 }
@@ -88,15 +91,45 @@ impl Not for U256 {
 
     fn not(self) -> Self {
         Self {
-            left: !self.left,
-            right: !self.right,
+            low: !self.low,
+            high: !self.high,
         }
+    }
+}
+
+impl BitOrAssign for U256 {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.low |= rhs.low;
+        self.high |= rhs.high;
     }
 }
 
 impl BitAndAssign for U256 {
     fn bitand_assign(&mut self, rhs: Self) {
-        self.left &= rhs.left;
-        self.right &= rhs.right;
+        self.low &= rhs.low;
+        self.high &= rhs.high;
     }
+}
+
+#[test]
+fn basics() {
+    let mut val = U256::default();
+    val.set_bit(0);
+    assert_eq!(val.high, 0);
+    assert_eq!(val.low, 1);
+
+    let mut val = U256::default();
+    val.set_bit(127);
+    assert_eq!(val.high, 0);
+    assert_eq!(val.low, 1 << 127);
+
+    let mut val = U256::default();
+    val.set_bit(128);
+    assert_eq!(val.high, 1);
+    assert_eq!(val.low, 0);
+
+    let mut val = U256::default();
+    val.set_bit(255);
+    assert_eq!(val.high, 1 << 127);
+    assert_eq!(val.low, 0);
 }
--- crates/core/src/year2021/day25.rs
+++ crates/core/src/year2021/day25.rs
@@ -1,103 +1,119 @@
+/// Categories: Bit manipulation
+use crate::common::u256::U256;
 use crate::input::Input;
 
+#[derive(Copy, Clone)]
+struct CucumberRow {
+    moving_east_bits: U256,
+    moving_south_bits: U256,
+}
+
+impl CucumberRow {
+    fn bits(self) -> U256 {
+        self.moving_east_bits | self.moving_south_bits
+    }
+}
+
+pub const fn shift_left(this: U256, width: usize) -> U256 {
+    if width <= 128 {
+        let mask = if width == 128 { !0 } else { !(1 << width) };
+        let low = ((this.low << 1) & mask) | (this.low >> (width - 1));
+        U256 {
+            high: this.high,
+            low,
+        }
+    } else {
+        // abcd efgh -> bcde efga
+        let mask = !(1 << (width - 128));
+        let high = ((this.high << 1) & mask) | (this.low >> 127);
+        let low = (this.low << 1) | (this.high >> (width - 129));
+        U256 { high, low }
+    }
+}
+
+pub const fn shift_right(this: U256, width: usize) -> U256 {
+    if width <= 128 {
+        let low = (this.low >> 1) | ((this.low & 1) << (width - 1));
+        U256 {
+            high: this.high,
+            low,
+        }
+    } else {
+        let high = (this.high >> 1) | ((this.low & 1) << (width - 129));
+        let low = (this.low >> 1) | ((this.high & 1) << 127);
+        U256 { high, low }
+    }
+}
+
 pub fn solve(input: &Input) -> Result<u32, String> {
-    const MAX_ITERATIONS: usize = 1000;
-    let mut region = Region::parse(input.text)?;
-    for round in 1..MAX_ITERATIONS {
-        if region.update_is_stable() {
-            return Ok(round as u32);
-        }
-    }
-    Err(format!("Did not stabilize in {MAX_ITERATIONS} iterations"))
-}
+    const MAX_ITERATIONS: u32 = 1000;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
-enum Location {
-    Empty,
-    CucumberFacingEast,
-    CucumberFacingSouth,
-}
+    let mut width = 0;
 
-pub struct Region {
-    locations: Vec<Location>,
-    new_locations: Vec<Location>,
-    width: u16,
-    height: u16,
-}
-
-impl Region {
-    fn parse(text: &str) -> Result<Self, String> {
-        let height = text.lines().count();
-        let width = text.lines().next().unwrap_or_default().len();
-        if height < 1 || width < 1 {
-            return Err("Too small input".to_string());
-        }
-
-        let mut locations = vec![Location::Empty; width * height];
-        for (y, line) in text.lines().enumerate() {
-            if line.len() != width {
-                return Err("Not all lines have equal length".to_string());
-            }
-            for (x, byte) in line.bytes().enumerate() {
-                let location = match byte {
-                    b'.' => Location::Empty,
-                    b'>' => Location::CucumberFacingEast,
-                    b'v' => Location::CucumberFacingSouth,
-                    _ => {
-                        return Err("Invalid input".to_string());
+    let mut cucumber_rows = input
+        .text
+        .lines()
+        .map(|line| {
+            let mut moving_east_bits = U256::default();
+            let mut moving_south_bits = U256::default();
+            for (offset, col) in line.bytes().enumerate() {
+                match col {
+                    b'>' => {
+                        moving_east_bits.set_bit(offset);
+                        width = width.max(offset + 1);
                     }
-                };
-                locations[x + y * width] = location;
+                    b'v' => {
+                        moving_south_bits.set_bit(offset);
+                        width = width.max(offset + 1);
+                    }
+                    _ => (),
+                }
             }
-        }
-        Ok(Self {
-            new_locations: locations.clone(),
-            locations,
-            width: width as u16,
-            height: height as u16,
+            CucumberRow {
+                moving_east_bits,
+                moving_south_bits,
+            }
         })
-    }
+        .collect::<Vec<_>>();
 
-    fn update_is_stable(&mut self) -> bool {
-        self.new_locations.fill(Location::Empty);
+    let height = cucumber_rows.len();
 
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let existing_idx = (x + y * self.width) as usize;
-                if Location::CucumberFacingEast == self.locations[existing_idx] {
-                    let new_x = (x + 1) % self.width;
-                    let new_idx = (new_x + y * self.width) as usize;
-                    self.new_locations[if matches!(self.locations[new_idx], Location::Empty) {
-                        new_idx
-                    } else {
-                        existing_idx
-                    }] = Location::CucumberFacingEast;
-                }
-            }
+    for step in 1..MAX_ITERATIONS {
+        let mut any_cucumber_moved = false;
+
+        // "Every step, the sea cucumbers in the east-facing herd attempt to move forward one location":
+        for row in cucumber_rows.iter_mut() {
+            let moved = shift_left(row.moving_east_bits, width) & !row.bits();
+            any_cucumber_moved |= moved.non_zero();
+            let stay = row.moving_east_bits & !shift_right(moved, width);
+            row.moving_east_bits = moved | stay;
         }
 
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let existing_idx = (x + y * self.width) as usize;
-                if Location::CucumberFacingSouth == self.locations[existing_idx] {
-                    let new_y = (y + 1) % self.height;
-                    let new_idx = (x + new_y * self.width) as usize;
-                    let is_new_location_empty =
-                        matches!(self.new_locations[new_idx], Location::Empty);
-                    self.new_locations[if is_new_location_empty
-                        && !matches!(self.locations[new_idx], Location::CucumberFacingSouth)
-                    {
-                        new_idx
-                    } else {
-                        existing_idx
-                    }] = Location::CucumberFacingSouth;
-                }
-            }
-        }
+        // "[..] then the sea cucumbers in the south-facing herd attempt to move forward one location":
+        let orig_first_row = cucumber_rows[0];
+        let mut moving_south_scratch_bits = U256::default();
+        for i in 0..height {
+            let row_to_the_south = if i == height - 1 {
+                orig_first_row
+            } else {
+                cucumber_rows[i + 1]
+            };
 
-        std::mem::swap(&mut self.locations, &mut self.new_locations);
-        self.locations == self.new_locations
+            // Everyone who wants to move south and can:
+            let moved = cucumber_rows[i].moving_south_bits & !row_to_the_south.bits();
+            any_cucumber_moved |= moved.non_zero();
+            cucumber_rows[i].moving_south_bits &= !moved;
+            cucumber_rows[i].moving_south_bits |= moving_south_scratch_bits;
+            moving_south_scratch_bits = moved;
+        }
+        cucumber_rows[0].moving_south_bits |= moving_south_scratch_bits;
+
+        if !any_cucumber_moved {
+            return Ok(step);
+        }
     }
+
+    Err(format!("Did not stabilize in {MAX_ITERATIONS} iterations"))
 }
 
 #[test]
@@ -117,4 +133,63 @@ v.v..>>v.v
 
     let real_input = include_str!("day25_input.txt");
     test_part_one!(real_input => 582);
+}
+
+#[test]
+pub fn test_shift_left() {
+    let mut val = U256 { low: 0, high: 0 };
+    val.set_bit(0);
+    assert_eq!(val.low, 1);
+    assert_eq!(val.high, 0);
+    val = shift_left(val, 10);
+    assert_eq!(val.low, 2);
+    assert_eq!(val.high, 0);
+    val = shift_left(val, 10);
+    assert_eq!(val.low, 4);
+    assert_eq!(val.high, 0);
+    val = shift_left(val, 4);
+    assert_eq!(val.low, 8);
+    assert_eq!(val.high, 0);
+    val = shift_left(val, 4);
+    assert_eq!(val.low, 1);
+    assert_eq!(val.high, 0);
+
+    val.low = 1 << 127;
+    val = shift_left(val, 128);
+    assert_eq!(val.low, 1);
+    assert_eq!(val.high, 0);
+
+    val.low = 1 << 127;
+    val = shift_left(val, 129);
+    assert_eq!(val.low, 0);
+    assert_eq!(val.high, 1);
+    val = shift_left(val, 129);
+    assert_eq!(val.low, 1);
+    assert_eq!(val.high, 0);
+}
+
+#[test]
+pub fn test_shift_right() {
+    let mut val = U256 { low: 0, high: 0 };
+    val.set_bit(0);
+    assert_eq!(val.low, 1);
+    assert_eq!(val.high, 0);
+    val = shift_right(val, 4);
+    assert_eq!(val.low, 8);
+    assert_eq!(val.high, 0);
+    val = shift_right(val, 4);
+    assert_eq!(val.low, 4);
+    assert_eq!(val.high, 0);
+    val.low = 1;
+    val = shift_right(val, 128);
+    assert_eq!(val.low, 1 << 127);
+    assert_eq!(val.high, 0);
+    val.low = 1;
+    val.high = 0;
+    val = shift_right(val, 129);
+    assert_eq!(val.low, 0);
+    assert_eq!(val.high, 1);
+    val = shift_right(val, 129);
+    assert_eq!(val.low, 1 << 127);
+    assert_eq!(val.high, 0);
 }
