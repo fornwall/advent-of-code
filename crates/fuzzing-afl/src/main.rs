d23cc2d4f46327751fed0cbf570d6839adc8a89c
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-03 00:53:44 +0100

    Fuzz 2023

--- crates/fuzzing-afl/src/main.rs
+++ crates/fuzzing-afl/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     fuzz!(|data: &[u8]| {
         if data.len() >= 4 {
             if let Ok(input_string) = std::str::from_utf8(&data[3..]) {
-                let year = 2015 + u16::from(data[0] % 8);
+                let year = 2015 + u16::from(data[0] % 9);
                 let day = 1 + data[1] % 25;
                 let part = 1 + data[2] % 2;
                 let _ = solve(year, day, part, input_string);
--- crates/fuzzing-libfuzzer/fuzz/fuzz_targets/fuzz_target.rs
+++ crates/fuzzing-libfuzzer/fuzz/fuzz_targets/fuzz_target.rs
@@ -6,7 +6,7 @@ use advent_of_code::solve;
 fuzz_target!(|data: &[u8]| {
     if data.len() >= 4 {
         if let Ok(input_string) = std::str::from_utf8(&data[3..]) {
-            let year = 2015 + (data[0] % 8) as u16;
+            let year = 2015 + (data[0] % 9) as u16;
             let day = 1 + data[1] % 25;
             let part = 1 + data[2] % 2;
             let _ = solve(year, day, part, input_string);
