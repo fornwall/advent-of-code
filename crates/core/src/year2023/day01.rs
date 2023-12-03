586cbf19a832807cd09b3f8eb872d75b991755a7
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-03 03:00:39 +0100

    Simplify 2023-01

--- crates/core/src/year2023/day01.rs
+++ crates/core/src/year2023/day01.rs
@@ -15,93 +15,38 @@ fn calibration_value(line: &str, part2: bool) -> u32 {
     let mut bits = 0_u64;
     for byte in line.bytes() {
         bits = bits << 8 | u64::from(byte);
-        for &(check_bits, val, mask) in [
-            (u64::from(b'1'), 1_u32, 0xff),
-            (u64::from(b'2'), 2, 0xff),
-            (u64::from(b'3'), 3, 0xff),
-            (u64::from(b'4'), 4, 0xff),
-            (u64::from(b'5'), 5, 0xff),
-            (u64::from(b'6'), 6, 0xff),
-            (u64::from(b'7'), 7, 0xff),
-            (u64::from(b'8'), 8, 0xff),
-            (u64::from(b'9'), 9, 0xff),
-            (
-                u64::from(b'o') << 16 | u64::from(b'n') << 8 | u64::from(b'e'),
-                1,
-                0x00ff_ffff,
-            ),
-            (
-                u64::from(b't') << 16 | u64::from(b'w') << 8 | u64::from(b'o'),
-                2,
-                0x00ff_ffff,
-            ),
-            (
-                u64::from(b't') << 32
-                    | u64::from(b'h') << 24
-                    | u64::from(b'r') << 16
-                    | u64::from(b'e') << 8
-                    | u64::from(b'e'),
-                3,
-                0xff_ffff_ffff,
-            ),
-            (
-                u64::from(b'f') << 24
-                    | u64::from(b'o') << 16
-                    | u64::from(b'u') << 8
-                    | u64::from(b'r'),
-                4,
-                0xffff_ffff,
-            ),
-            (
-                u64::from(b'f') << 24
-                    | u64::from(b'i') << 16
-                    | u64::from(b'v') << 8
-                    | u64::from(b'e'),
-                5,
-                0xffff_ffff,
-            ),
-            (
-                u64::from(b's') << 16 | u64::from(b'i') << 8 | u64::from(b'x'),
-                6,
-                0xff_ffff,
-            ),
-            (
-                u64::from(b's') << 32
-                    | u64::from(b'e') << 24
-                    | u64::from(b'v') << 16
-                    | u64::from(b'e') << 8
-                    | u64::from(b'n'),
-                7,
-                0xff_ffff_ffff,
-            ),
-            (
-                u64::from(b'e') << 32
-                    | u64::from(b'i') << 24
-                    | u64::from(b'g') << 16
-                    | u64::from(b'h') << 8
-                    | u64::from(b't'),
-                8,
-                0xff_ffff_ffff,
-            ),
-            (
-                u64::from(b'n') << 24
-                    | u64::from(b'i') << 16
-                    | u64::from(b'n') << 8
-                    | u64::from(b'e'),
-                9,
-                0xffff_ffff,
-            ),
-        ]
-        .iter()
-        .take(if part2 { 18 } else { 9 })
-        {
-            if mask & bits == check_bits {
-                if first_digit.is_none() {
-                    first_digit = Some(val);
-                }
-                last_digit = val;
+        let (m1, m2, m3) = if part2 {
+            (0x00ff_ffff, 0xffff_ffff, 0xff_ffff_ffff)
+        } else {
+            (0, 0, 0)
+        };
+        let digit = match (bits & 0xff, bits & m1, bits & m2, bits & m3) {
+            (0b110001, _, _, _) => 1,
+            (0b110010, _, _, _) => 2,
+            (0b110011, _, _, _) => 3,
+            (0b110100, _, _, _) => 4,
+            (0b110101, _, _, _) => 5,
+            (0b110110, _, _, _) => 6,
+            (0b110111, _, _, _) => 7,
+            (0b111000, _, _, _) => 8,
+            (0b111001, _, _, _) => 9,
+            (_, 0b11011110110111001100101, _, _) => 1,
+            (_, 0b11101000111011101101111, _, _) => 2,
+            (_, _, _, 0b111010001101000011100100110010101100101) => 3,
+            (_, _, 0b1100110011011110111010101110010, _) => 4,
+            (_, _, 0b1100110011010010111011001100101, _) => 5,
+            (_, 0b11100110110100101111000, _, _) => 6,
+            (_, _, _, 0b111001101100101011101100110010101101110) => 7,
+            (_, _, _, 0b110010101101001011001110110100001110100) => 8,
+            (_, _, 0b1101110011010010110111001100101, _) => 9,
+            _ => {
+                continue;
             }
+        };
+        if first_digit.is_none() {
+            first_digit = Some(digit);
         }
+        last_digit = digit;
     }
 
     first_digit.unwrap_or_default() * 10 + last_digit
