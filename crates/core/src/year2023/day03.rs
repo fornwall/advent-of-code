f2d04f210a27218d32ad80e6eb04ed02b73071f4
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-03 15:47:35 +0100

    2023-03-2

--- crates/core/src/year2023/day03.rs
+++ crates/core/src/year2023/day03.rs
@@ -1,19 +1,29 @@
-use crate::common::u256::U256;
 use crate::input::Input;
 
 pub fn solve(input: &Input) -> Result<u64, String> {
     const MAP_SIZE: usize = 256;
-    let mut symbols = vec![U256::default(); MAP_SIZE];
+    let mut numbers = vec![0; MAP_SIZE * MAP_SIZE];
 
     for (row_idx, row_str) in input.text.lines().enumerate() {
         if row_idx >= MAP_SIZE || row_str.len() >= MAP_SIZE {
             return Err("Too big schematic".to_string());
         }
 
-        for (col_idx, col_byte) in row_str.bytes().enumerate() {
-            let is_symbol = !(col_byte.is_ascii_digit() || col_byte == b'.');
-            if is_symbol {
-                symbols[row_idx].set_bit(col_idx);
+        let mut num_start_idx = None;
+        for (col_idx, col_byte) in row_str.bytes().chain(std::iter::once(b'.')).enumerate() {
+            if col_byte.is_ascii_digit() {
+                if num_start_idx.is_none() {
+                    num_start_idx = Some(col_idx);
+                }
+            } else if let Some(start_idx) = num_start_idx {
+                let num_str = &row_str[start_idx..col_idx];
+                let num = num_str
+                    .parse::<u16>()
+                    .map_err(|_| "Invalid number".to_string())?;
+                for i in start_idx..col_idx {
+                    numbers[row_idx * MAP_SIZE + i] = num;
+                }
+                num_start_idx = None;
             }
         }
     }
@@ -21,26 +31,45 @@ pub fn solve(input: &Input) -> Result<u64, String> {
     let mut sum = 0;
 
     for (row_idx, row_str) in input.text.lines().enumerate() {
-        let mut num_start_idx = None;
         for (col_idx, col_byte) in row_str.bytes().chain(std::iter::once(b'.')).enumerate() {
-            if col_byte.is_ascii_digit() {
-                if num_start_idx.is_none() {
-                    num_start_idx = Some(col_idx);
-                }
+            let interesting = if input.is_part_one() {
+                !col_byte.is_ascii_digit() && col_byte != b'.'
             } else {
-                if let Some(start_idx) = num_start_idx {
-                    let interval_start_col = start_idx.saturating_sub(1);
-                    let interval_end_col = col_idx;
-                    let adjacent_to_symbol =
-                        symbols[row_idx.saturating_sub(1)].range_non_zero(interval_start_col..=interval_end_col)
-                            || symbols[row_idx].range_non_zero(interval_start_col..=interval_end_col)
-                            || symbols[row_idx + 1].range_non_zero(interval_start_col..=interval_end_col);
-                    if adjacent_to_symbol {
-                        let num_str = &row_str[start_idx..col_idx];
-                        sum += num_str.parse::<u16>().unwrap() as u64;
+                col_byte == b'*'
+            };
+            if interesting {
+                let mut num_neighbour_count = 0;
+                let mut this_value = input.part_values(0, 1);
+                for dx in -1_i32..=1 {
+                    for dy in -1_i32..=1 {
+                        if (col_idx, dx) == (0, -1)
+                            || (row_idx, dy) == (0, -1)
+                            || (dx == 0 && dy == 0)
+                        {
+                            continue;
+                        }
+                        let x = col_idx as i32 + dx;
+                        let y = row_idx as i32 + dy;
+                        if matches!((dx, dy), (0 | 1, -1 | 1))
+                            && numbers[(y as usize) * MAP_SIZE + (x - 1) as usize] != 0
+                        {
+                            // Avoid counting same number multiple times.
+                            continue;
+                        }
+                        let adjacent_num = numbers[y as usize * MAP_SIZE + x as usize];
+                        if input.is_part_one() {
+                            this_value += u64::from(adjacent_num);
+                        } else if adjacent_num != 0 {
+                            num_neighbour_count += 1;
+                            this_value *= u64::from(adjacent_num);
+                        }
                     }
-                    num_start_idx = None;
                 }
+                sum += if input.is_part_one() || (num_neighbour_count == 2) {
+                    this_value
+                } else {
+                    0
+                };
             }
         }
     }
@@ -50,7 +79,7 @@ pub fn solve(input: &Input) -> Result<u64, String> {
 
 #[test]
 pub fn tests() {
-    use crate::input::{test_part_one_no_allocations, test_part_two_no_allocations};
+    use crate::input::{test_part_one_num_allocations, test_part_two_num_allocations};
 
     let test_input = "467..114..
 ...*......
@@ -62,10 +91,11 @@ pub fn tests() {
 ......755.
 ...$.*....
 .664.598..";
-    test_part_one_no_allocations!(test_input => 4361);
-    test_part_two_no_allocations!(test_input => 467835);
+
+    test_part_one_num_allocations!(test_input => 4361, 1);
+    test_part_two_num_allocations!(test_input => 467_835, 1);
 
     let real_input = include_str!("day03_input.txt");
-    test_part_one_no_allocations!(real_input => 546563);
-    //test_part_two_no_allocations!(real_input => 0);
+    test_part_one_num_allocations!(real_input => 546_563, 1);
+    test_part_two_num_allocations!(real_input => 91_031_374, 1);
 }
