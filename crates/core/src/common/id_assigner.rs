410fb45231558c7060c71e607236a66060fc6a20
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-08 23:32:29 +0100

    id_assigner stack

--- crates/core/src/common/id_assigner.rs
+++ crates/core/src/common/id_assigner.rs
@@ -1,21 +1,24 @@
 use std::hash::Hash;
 
-pub struct IdAssigner<'a, const MAX_SIZE: usize, H: Hash + Eq + ?Sized> {
-    id_map: std::collections::hash_map::HashMap<&'a H, u16>,
-    //id_map: [&'a H; MAX_SIZE],
-    //assigned_count: u16,
+pub struct IdAssigner<'a, const MAX_SIZE: usize, H: Ord + Eq + ?Sized> {
+    //id_map: std::collections::hash_map::HashMap<&'a H, u16>,
+    id_map: [&'a H; MAX_SIZE],
+    ids: [u16; MAX_SIZE],
+    assigned_count: u16,
 }
 
-impl<'a, const MAX_SIZE: usize, H: Hash + Eq + ?Sized> IdAssigner<'a, MAX_SIZE, H> {
+impl<'a, const MAX_SIZE: usize, H: Ord + Eq + ?Sized> IdAssigner<'a, MAX_SIZE, H> {
     pub fn new(default: &'a H) -> Self {
         Self {
-            id_map: Default::default(),
-            //id_map: [default; MAX_SIZE],
-            //assigned_count: 0,
+            //id_map: Default::default(),
+            id_map: [default; MAX_SIZE],
+            ids: [0; MAX_SIZE],
+            assigned_count: 0,
         }
     }
 
     pub fn id_of(&mut self, name: &'a H) -> Result<u16, String> {
+        /*
         let next_id = self.id_map.len() as u16;
         Ok(match self.id_map.entry(name) {
             std::collections::hash_map::Entry::Vacant(entry) => {
@@ -27,39 +30,39 @@ impl<'a, const MAX_SIZE: usize, H: Hash + Eq + ?Sized> IdAssigner<'a, MAX_SIZE, 
             }
             std::collections::hash_map::Entry::Occupied(e) => *e.get(),
         })
+         */
 
-        /*
-                for (idx, &el) in self.id_map.iter().enumerate() {
-            if el == name {
-                return Ok(idx as u16);
-            }
-        }
-        if usize::from(self.assigned_count) == MAX_SIZE {
-            return Err("Too many entries".to_string());
-        }
-        let new_id = self.assigned_count;
-        self.assigned_count += 1;
-        self.id_map[new_id as usize] = name;
-        Ok(new_id)
-
-                     */
+        Ok(
+            match (&self.id_map[0..(self.assigned_count as usize)]).binary_search(&name) {
+                Ok(idx) => self.ids[idx],
+                Err(idx) => {
+                    self.id_map
+                        .copy_within(idx..self.assigned_count as usize, idx + 1);
+                    self.ids
+                        .copy_within(idx..self.assigned_count as usize, idx + 1);
+                    let new_id = self.assigned_count;
+                    self.assigned_count += 1;
+                    self.id_map[idx] = name;
+                    self.ids[idx] = new_id;
+                    new_id
+                }
+            },
+        )
     }
 
     pub fn get_id(&mut self, name: &H) -> Option<u16> {
-        self.id_map.get(name).copied()
-        /*
-        for (idx, &el) in self.id_map.iter().enumerate() {
-            if el == name {
-                return Some(idx as u16);
-            }
+        //self.id_map.get(name).copied()
+
+        if let Ok(idx) = (&self.id_map[0..(self.assigned_count as usize)]).binary_search(&name) {
+            Some(self.ids[idx])
+        } else {
+            None
         }
-        None
-             */
     }
 
     pub fn len(&self) -> usize {
-        self.id_map.len()
-        //self.assigned_count as usize
+        //self.id_map.len()
+        self.assigned_count as usize
     }
 }
 
