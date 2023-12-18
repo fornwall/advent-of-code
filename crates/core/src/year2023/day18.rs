9487556c011c9db3d648c924259d161753b27bb7
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-18 21:01:10 +0100

    2023-18-2

--- crates/core/src/year2023/day18.rs
+++ crates/core/src/year2023/day18.rs
@@ -1,88 +1,53 @@
 use crate::input::{on_error, Input};
 
-pub fn solve(input: &Input) -> Result<u64, String> {
-    const X_OFFSET: usize = 250;
-    const Y_OFFSET: usize = 20;
-    const GRID_WIDTH: usize = 520;
-    const GRID_HEIGHT: usize = 260;
-
-    let (mut x, mut y) = (X_OFFSET, Y_OFFSET);
-    let (mut max_x, mut max_y) = (usize::MIN, usize::MIN);
-    let (mut min_x, mut min_y) = (usize::MAX, usize::MAX);
-    let mut visited = [false; GRID_WIDTH * GRID_HEIGHT];
+pub fn solve(input: &Input) -> Result<i64, String> {
+    let (mut x, mut y) = (0, 0);
+    let (mut area_sum, mut trench_len) = (0_i64, 0_i64);
 
     for line in input.text.lines() {
         let mut parts = line.split(' ');
-        let direction = parts.next().ok_or_else(on_error)?;
-        let amount = parts
-            .next()
-            .ok_or_else(on_error)?
-            .parse::<i32>()
-            .map_err(|_| on_error())?;
-        let color = parts.next().ok_or_else(on_error)?;
-
         let (direction, amount) = if input.is_part_one() {
-            (direction, amount)
-        } else {
-            let direction = match color.as_bytes()[7] {
-                b'0' => "R",
-                b'1' => "D",
-                b'2' => "L",
-                _ => "U",
-            };
+            let first = parts.next().ok_or_else(on_error)?;
+            let second = parts.next().ok_or_else(on_error)?;
             (
-                direction,
-                i32::from_str_radix(&color[2..7], 16).map_err(|_| on_error())?,
+                first.as_bytes()[0],
+                second.parse::<i64>().map_err(|_| on_error())?,
+            )
+        } else {
+            let color = parts.nth(2).ok_or_else(on_error)?;
+            (
+                color.as_bytes()[7],
+                i64::from_str_radix(&color[2..7], 16).map_err(|_| on_error())?,
             )
         };
 
-        match direction {
-            "U" => {
-                for _ in 0..amount {
-                    y += 1;
-                    visited[(y * GRID_WIDTH) + x] = true;
-                }
-            }
-            "R" => {
-                for _ in 0..amount {
-                    x += 1;
-                    visited[(y * GRID_WIDTH) + x] = true;
-                }
-            }
-            "D" => {
-                for _ in 0..amount {
-                    y -= 1;
-                    visited[(y * GRID_WIDTH) + x] = true;
-                }
-            }
-            _ => {
-                for _ in 0..amount {
-                    x -= 1;
-                    visited[(y * GRID_WIDTH) + x] = true;
-                }
-            }
-        }
-        min_x = min_x.min(x);
-        max_x = max_x.max(x);
-        min_y = min_y.min(y);
-        max_y = max_y.max(y);
+        let (new_x, new_y) = match direction {
+            b'U' | b'0' => (x, y + amount),
+            b'R' | b'1' => (x + amount, y),
+            b'D' | b'2' => (x, y - amount),
+            _ => (x - amount, y),
+        };
+
+        area_sum += x * new_y - new_x * y;
+        trench_len += (new_x - x).abs() + (new_y - y).abs();
+        (x, y) = (new_x, new_y);
     }
 
-    let mut result = 0;
-    for y in (min_y..=max_y).rev() {
-        let mut inside_loop = false;
-        for x in min_x..=max_x {
-            //println!("{},{} is inside? {}", x, y, visited[(y * GRID_WIDTH) + x]);
-            //print!("{}", if visited[(y * GRID_WIDTH) + x] { '#' } else { '.' });
-            let this_is_edge = visited[(y * GRID_WIDTH) + x];
-
-            if this_is_edge && visited[(y + 1) * GRID_WIDTH + x] {
-                inside_loop = !inside_loop;
-            }
-            result += u64::from(this_is_edge | inside_loop);
-        }
-    }
-    Ok(result)
+    // https://en.wikipedia.org/wiki/Shoelace_formula#Triangle_formula:
+    let area_inside = area_sum.abs() / 2;
+    // In addition to the area inside of the trench path, we need to count
+    // the 1/2 wide areas outside of the border:
+    //    12
+    // 8 ---
+    // 7 | | 3
+    //   --- 4
+    //   65
+    // With the above sketch starting from the top right, there are 1/4
+    // square missed for each clockwise turn, and a 1/4 of a square double
+    // counted for each counterclockwise turn.
+    // Since this is a loop there are 4 more clockwise turns -> add 4*1/4=1.
+    let area_from_border = trench_len / 2 + 1;
+    Ok(area_inside + area_from_border)
 }
 
 #[test]
@@ -104,9 +69,9 @@ U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)";
     test_part_one_no_allocations!(test_input => 62);
-    //test_part_two_no_allocations!(test_input => 0);
+    test_part_two_no_allocations!(test_input => 952_408_144_115);
 
     let real_input = include_str!("day18_input.txt");
     test_part_one_no_allocations!(real_input => 50_746);
-    //test_part_two_no_allocations!(real_input => 0);
+    test_part_two_no_allocations!(real_input => 70_086_216_556_038);
 }
