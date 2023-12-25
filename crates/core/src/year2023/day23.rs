4fc4a22b996e5c8747586802e77ed71a95704154
Fredrik Fornwall <fredrik@fornwall.net>
2023-12-25 01:00:55 +0100

    2023-23 with initial contraction

--- crates/core/src/year2023/day23.rs
+++ crates/core/src/year2023/day23.rs
@@ -1,58 +1,117 @@
-use crate::common::array_deque::ArrayDeque;
 use crate::common::array_stack::ArrayStack;
-use crate::common::u256::U256;
+use crate::common::id_assigner_copy::IdAssigner;
 use crate::input::{on_error, Input};
 
 pub fn solve(input: &Input) -> Result<u16, String> {
-    let mut map = Map::parse(input.text.as_bytes())?;
-    let mut visited = [U256::default(); 200];
-    let mut longest = 0;
+    const MAX_COMPACTED_GRAPH_LEN: usize = 60;
 
-    let mut work_queue = ArrayStack::<1000, (u16, u16, u16)>::new();
-    work_queue.push((1, 0, 0))?;
+    let map = Map::parse(input.text.as_bytes())?;
 
-    let mut undo_stack = ArrayStack::<10000, (u16, u16)>::new();
+    let mut compacted_graph = [([(0_u16, 0_u16); 5], 0_u8); MAX_COMPACTED_GRAPH_LEN];
+    let mut id_assigner = IdAssigner::<MAX_COMPACTED_GRAPH_LEN, (u16, u16)>::new((0, 0));
+    let mut work_queue = ArrayStack::<1000, (u16, u16, u16, u16, u16, u16)>::new();
 
-    while let Some((x, y, path_len)) = work_queue.pop() {
-        if x as usize == map.num_cols - 2 && y as usize == map.num_rows - 1 {
-            longest = longest.max(path_len);
-            continue;
-        }
+    let src_idx = id_assigner.id_of((1, 0))?;
+    work_queue.push((1, 0, 0, 0, 0, src_idx))?;
+    let mut destination_idx = 0;
 
-        while undo_stack.len() > path_len as usize {
-            let (undo_x, undo_y) = undo_stack.pop_unwrap();
-            visited[undo_y as usize].clear_bit(undo_x as usize);
-        }
-
+    while let Some((x, y, from_x, from_y, path_len, compact_src_idx)) = work_queue.pop() {
         let current_square = map.get(x as usize, y as usize);
+        let mut possible_paths = ArrayStack::<3, (u16, u16)>::new();
+
         for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            if input.is_part_one() {
-                if (current_square == b'>' && dx != 1)
+            if input.is_part_one()
+                && ((current_square == b'>' && dx != 1)
                     || (current_square == b'<' && dx != -1)
                     || (current_square == b'^' && dy != -1)
-                    || (current_square == b'v' && dy != 1) {
-                    continue;
+                    || (current_square == b'v' && dy != 1))
+            {
+                continue;
+            }
+            let new_x = i32::from(x) + dx;
+            let new_y = i32::from(y) + dy;
+            if new_x < 0
+                || new_x >= map.num_cols as i32
+                || new_y < 0
+                || new_y >= map.num_rows as i32
+                || (new_x as u16 == from_x && new_y as u16 == from_y)
+                || map.get(new_x as usize, new_y as usize) == b'#'
+            {
+                continue;
+            }
+            possible_paths.push((new_x as u16, new_y as u16))?;
+        }
+
+        let at_destination = x as usize == (map.num_cols - 2) && y as usize == (map.num_rows - 1);
+
+        let (new_compact_node_idx, new_path_len) = if possible_paths.len() > 1 || at_destination {
+            // A new node in the compacted graph.
+            let orig_len = id_assigner.len();
+            let new_idx = id_assigner.id_of((x, y))?;
+            let visited_before = orig_len == id_assigner.len();
+
+            let mut existing_path_found = false;
+            for &(from, to) in [(compact_src_idx, new_idx), (new_idx, compact_src_idx)]
+                .iter()
+                .take(input.part_values(1, 2))
+            {
+                let src_node = &mut compacted_graph[from as usize];
+                for i in 0..src_node.1 {
+                    if src_node.0[i as usize].0 == to {
+                        existing_path_found = true;
+                        src_node.0[i as usize].1 = src_node.0[i as usize].1.max(path_len);
+                    }
+                }
+                if !existing_path_found {
+                    src_node.0[src_node.1 as usize] = (to, path_len);
+                    src_node.1 += 1;
                 }
             }
-            let new_x = (x as i32) + dx;
-            let new_y = (y as i32) + dy;
-            if new_x < 0 || new_x >= map.num_cols as i32
-                || new_y < 0 || new_y >= map.num_rows as i32
-                || map.get(new_x as usize, new_y as usize) == b'#' {
+
+            if at_destination {
+                destination_idx = new_idx;
                 continue;
             }
-            if visited[new_y as usize].is_bit_set(new_x as usize) {
+
+            if visited_before {
                 continue;
             }
-            undo_stack.push((new_x as u16, new_y as u16))?;
-            visited[new_y as usize].set_bit(new_x as usize);
-            work_queue.push((new_x as u16, new_y as u16, path_len + 1))?;
+            (new_idx, 1)
+        } else {
+            // Not a new node in the compacted graph.
+            (compact_src_idx, path_len + 1)
+        };
+
+        while let Some((new_x, new_y)) = possible_paths.pop() {
+            work_queue.push((new_x, new_y, x, y, new_path_len, new_compact_node_idx))?;
+        }
+    }
+
+    let mut longest = 0;
+    let mut work_queue = ArrayStack::<1000, (u16, u64, u16)>::new();
+    work_queue.push((src_idx, 1 << src_idx, 0))?;
+    while let Some((idx, visited_bitmask, path_len)) = work_queue.pop() {
+        let node = compacted_graph[idx as usize];
+        for i in 0..node.1 {
+            let (edge_destination_idx, edge_len) = node.0[i as usize];
+            let edge_destination_bit = 1 << edge_destination_idx;
+            if edge_destination_idx == destination_idx {
+                longest = longest.max(path_len + edge_len);
+                continue;
+            }
+            if visited_bitmask & edge_destination_bit == 0 {
+                let new_visited_bitmask = visited_bitmask | edge_destination_bit;
+                work_queue.push((
+                    edge_destination_idx,
+                    new_visited_bitmask,
+                    path_len + edge_len,
+                ))?;
+            }
         }
     }
 
     Ok(longest)
 }
-
 
 struct Map<'a> {
     bytes: &'a [u8],
@@ -84,7 +143,6 @@ impl<'a> Map<'a> {
     const fn get(&self, x: usize, y: usize) -> u8 {
         self.bytes[self.xy_to_idx(x, y)]
     }
-
 }
 
 #[test]
@@ -119,5 +177,5 @@ pub fn tests() {
 
     let real_input = include_str!("day23_input.txt");
     test_part_one_no_allocations!(real_input => 2042);
-    //test_part_two_no_allocations!(real_input => 0);
+    test_part_two_no_allocations!(real_input => 6466);
 }
