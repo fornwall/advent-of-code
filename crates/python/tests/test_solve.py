d6827c0ae3ec143945621b6f0b0ba77023c94a81
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-03 10:23:47 +0200

    Remove unittest import

--- crates/python/tests/test_solve.py
+++ crates/python/tests/test_solve.py
@@ -1,5 +1,3 @@
-import unittest
-
 import advent_of_code
 
 assert advent_of_code.solve("2019", "1", "1", "12") == "2"
@@ -8,8 +6,3 @@ assert advent_of_code.solve(2019, 1, 1, "14") == "2"
 assert advent_of_code.solve(2019, 1, 1, "1969") == "654"
 assert advent_of_code.solve(2019, 1, 1, "100756") == "33583"
 assert advent_of_code.solve(2019, 9, 1, "104,1125899906842624,99") == "1125899906842624"
-
-
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
