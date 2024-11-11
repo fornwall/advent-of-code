45d4bf881c25e6cbf2c600f2aca76c458a25ae8c
Fredrik Fornwall <fredrik@fornwall.net>
2024-11-11 23:48:31 +0100

    Use maneatingape:s solutions for 2023-24 and 2023-25

--- crates/core/src/year2023/day24.rs
+++ crates/core/src/year2023/day24.rs
@@ -1,298 +1,195 @@
-use crate::common::array_stack::ArrayStack;
-use crate::input::{on_error, Input};
+use crate::{
+    common::array_stack::ArrayStack,
+    input::{on_error, Input},
+};
+
 use std::ops::RangeInclusive;
 
-const D: usize = 6;
+const RANGE: RangeInclusive<i64> = 200_000_000_000_000..=400_000_000_000_000;
 
-pub fn solve(input: &Input) -> Result<u64, String> {
-    let mut particles = ArrayStack::<512, Particle>::new();
-    for line in input.text.lines() {
-        let (position_str, velocity_str) = line.split_once(" @ ").ok_or_else(on_error)?;
-        particles.push(Particle {
-            position: Coordinate::parse(position_str)?,
-            velocity: Coordinate::parse(velocity_str)?,
-        })?;
+pub const fn gcd(mut a: i128, mut b: i128) -> i128 {
+    while b != 0 {
+        let tmp = a;
+        a = b;
+        b = tmp % b;
     }
-
-    if input.is_part_one() {
-        let mut count = 0;
-        let interval = 200_000_000_000_000..=400_000_000_000_000;
-        for (i, a) in particles.slice().iter().enumerate() {
-            for b in &particles.elements[i + 1..particles.len()] {
-                if a.intersects_xy_with(*b, &interval) {
-                    count += 1;
-                }
-            }
-        }
-        Ok(count)
-    } else {
-        // From https://raw.githubusercontent.com/sebastianotronto/aoc/master/2023/24/24b.c
-
-        // "To figure the correct starting point + velocity, we solve some systems of linear equations.
-        // Write your starting point and velocity as unknowns x, y, z, Vx, Vy, Vz.
-        // Equating the position of the rock at time t1 (another unknown parameter) with the position
-        // of one of the hailstones at the same time t1, we get a system of 3 equations and 7 unknowns.
-        // Unfortunately, these equations have degree 2 - this is not a linear system.
-        // However, manipulating these equations a bit we can get a linear equation of the type:
-        //     (Vy1-Vy2)x - (Vx1-Vx2)y + - (y1-y2)Vx + (x1-x2)Vy = y2Vx2 + x2Vy2 - y1Vx1 - x1Vy1
-        // Where x1, y1, z2, Vx1, Vy1, Vz1 and x2, y2, z2, Vx2, Vy2, Vz2
-        // are the starting points and velocities of two of the hailstones.
-        // So with 2 lines we can get a linear equation. Similarly, we can
-        // get equations involving the unknowns z and Vz.
-        // We can use the myriad of hailstones we have to generate as many
-        // equations as we like. The system is going to be overdetermined,
-        // but the problem statement seems to ensure that there is going to
-        // be a solution. On the other hand it can happen that we make a
-        // bad choice of lines and the equation we use are underdetermined.
-        // This last problem is not accounted for in the code - if it happens,
-        // one can shuffle the input file until it works."
-        let l = [
-            particles.elements[0].to_float(),
-            particles.elements[1].to_float(),
-            particles.elements[2].to_float(),
-        ];
-
-        let a: [[f64; D]; D] = [
-            // First equation: Particles 0 and 1, x and y only.
-            [
-                l[0].v.y - l[1].v.y,
-                l[1].v.x - l[0].v.x,
-                0.0,
-                l[1].p.y - l[0].p.y,
-                l[0].p.x - l[1].p.x,
-                0.0,
-            ],
-            // Second equation: Particles 0 and 2, x and y only.
-            [
-                l[0].v.y - l[2].v.y,
-                l[2].v.x - l[0].v.x,
-                0.0,
-                l[2].p.y - l[0].p.y,
-                l[0].p.x - l[2].p.x,
-                0.0,
-            ],
-            // Third equation: Particles 0 and 1, x and z only.
-            [
-                l[0].v.z - l[1].v.z,
-                0.0,
-                l[1].v.x - l[0].v.x,
-                l[1].p.z - l[0].p.z,
-                0.0,
-                l[0].p.x - l[1].p.x,
-            ],
-            // Fourth equation: Particles 0 and 2, x and z only.
-            [
-                l[0].v.z - l[2].v.z,
-                0.0,
-                l[2].v.x - l[0].v.x,
-                l[2].p.z - l[0].p.z,
-                0.0,
-                l[0].p.x - l[2].p.x,
-            ],
-            // Fifth equation: Particles 0 and 1, y and z only.
-            [
-                0.0,
-                l[0].v.z - l[1].v.z,
-                l[1].v.y - l[0].v.y,
-                0.0,
-                l[1].p.z - l[0].p.z,
-                l[0].p.y - l[1].p.y,
-            ],
-            // Sixth equation: Particles 0 and 2, y and z only.
-            [
-                0.0,
-                l[0].v.z - l[2].v.z,
-                l[2].v.y - l[0].v.y,
-                0.0,
-                l[2].p.z - l[0].p.z,
-                l[0].p.y - l[2].p.y,
-            ],
-        ];
-        #[allow(clippy::suboptimal_flops)]
-        let c: [f64; D] = [
-            // First equation: Particles 0 and 1, x and y only.
-            l[1].p.y * l[1].v.x - l[1].p.x * l[1].v.y - l[0].p.y * l[0].v.x + l[0].p.x * l[0].v.y,
-            // Second equation: Particles 0 and 2, x and y only.
-            l[2].p.y * l[2].v.x - l[2].p.x * l[2].v.y - l[0].p.y * l[0].v.x + l[0].p.x * l[0].v.y,
-            // Third equation: Particles 0 and 1, x and z only.
-            l[1].p.z * l[1].v.x - l[1].p.x * l[1].v.z - l[0].p.z * l[0].v.x + l[0].p.x * l[0].v.z,
-            // Fourth equation: Particles 0 and 2, x and z only.
-            l[2].p.z * l[2].v.x - l[2].p.x * l[2].v.z - l[0].p.z * l[0].v.x + l[0].p.x * l[0].v.z,
-            // Fifth equation: Particles 0 and 1, y and z only.
-            l[1].p.z * l[1].v.y - l[1].p.y * l[1].v.z - l[0].p.z * l[0].v.y + l[0].p.y * l[0].v.z,
-            // Sixth equation: Particles 0 and 2, y and z only.
-            l[2].p.z * l[2].v.y - l[2].p.y * l[2].v.z - l[0].p.z * l[0].v.y + l[0].p.y * l[0].v.z,
-        ];
-        if let Some([x, y, z]) = solve_system(a, c) {
-            Ok(x.round() as u64 + y.round() as u64 + z.round() as u64)
-        } else {
-            Err("No solution found".to_string())
-        }
-    }
+    a
 }
 
-fn float_eq(x: f64, y: f64) -> bool {
-    const EPSILON: f64 = 1e-10;
-    (x - y).abs() < EPSILON * (x.abs() + y.abs())
-}
-
-// Solve the linear system a*x = c with row reduction.
-// Return Some(c) if it has a unique solution, otherwise None.
-fn solve_system(mut a: [[f64; D]; D], mut c: [f64; D]) -> Option<[f64; 3]> {
-    // Row reduction.
-    for i in 0..D {
-        // Make first nonzero.
-        let mut imax = 0;
-        let mut maxi = 0.0;
-
-        for (j, aj) in a.iter().enumerate().skip(i) {
-            if aj[i].abs() > maxi {
-                maxi = aj[i].abs();
-                imax = j;
-            }
-        }
-
-        if float_eq(maxi, 0.0) {
-            return None;
-        }
-
-        c.swap(i, imax);
-
-        for j in 0..D {
-            let tmp = a[i][j];
-            a[i][j] = a[imax][j];
-            a[imax][j] = tmp;
-        }
-
-        // Reduce rows.
-        for ii in (i + 1)..D {
-            let r = a[ii][i] / a[i][i];
-            for k in i..D {
-                a[ii][k] -= r * a[i][k];
-            }
-            c[ii] -= r * c[i];
-        }
-    }
-
-    // Back substitution.
-    let mut x = c;
-    for i in (0..=(D - 1)).rev() {
-        for j in (i + 1)..D {
-            x[i] -= a[i][j] * x[j];
-        }
-        x[i] /= a[i][i];
-    }
-
-    Some([x[0], x[1], x[2]])
-}
-
-#[derive(Copy, Clone, Default)]
-struct Coordinate {
+#[derive(Clone, Copy)]
+struct Vector {
     x: i128,
     y: i128,
-    #[allow(dead_code)]
     z: i128,
 }
 
-#[derive(Copy, Clone, Default)]
-struct FloatCoordinate {
-    x: f64,
-    y: f64,
-    z: f64,
+/// 3D vector implementation.
+impl Vector {
+    const fn add(self, other: Self) -> Self {
+        let x = self.x + other.x;
+        let y = self.y + other.y;
+        let z = self.z + other.z;
+        Self { x, y, z }
+    }
+
+    const fn sub(self, other: Self) -> Self {
+        let x = self.x - other.x;
+        let y = self.y - other.y;
+        let z = self.z - other.z;
+        Self { x, y, z }
+    }
+
+    const fn cross(self, other: Self) -> Self {
+        let x = self.y * other.z - self.z * other.y;
+        let y = self.z * other.x - self.x * other.z;
+        let z = self.x * other.y - self.y * other.x;
+        Self { x, y, z }
+    }
+
+    // Changes the magnitude (but not direction) of the vector.
+    // Prevents numeric overflow.
+    const fn gcd(self) -> Self {
+        let gcd = gcd(gcd(self.x, self.y), self.z);
+        let x = self.x / gcd;
+        let y = self.y / gcd;
+        let z = self.z / gcd;
+        Self { x, y, z }
+    }
+
+    const fn sum(self) -> i128 {
+        self.x + self.y + self.z
+    }
 }
 
-#[derive(Copy, Clone, Default)]
-struct FloatParticle {
-    p: FloatCoordinate,
-    v: FloatCoordinate,
-}
-
-impl Coordinate {
-    fn parse(s: &str) -> Result<Self, String> {
-        let mut parts = s.split(", ");
-        Ok(Self {
-            x: parts
+/// Solution from https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2023/day25.rs
+pub fn solve(input: &Input) -> Result<u64, String> {
+    let mut i = ArrayStack::<512, [i64; 6]>::new();
+    for line in input.text.lines() {
+        let (position_str, velocity_str) = line.split_once(" @ ").ok_or_else(on_error)?;
+        let mut p_parts = position_str.split(", ");
+        let mut v_parts = velocity_str.split(", ");
+        i.push([
+            p_parts
                 .next()
                 .ok_or_else(on_error)?
-                .trim()
                 .parse()
                 .map_err(|_| on_error())?,
-            y: parts
+            p_parts
                 .next()
                 .ok_or_else(on_error)?
-                .trim()
                 .parse()
                 .map_err(|_| on_error())?,
-            z: parts
+            p_parts
                 .next()
                 .ok_or_else(on_error)?
-                .trim()
                 .parse()
                 .map_err(|_| on_error())?,
-        })
+            v_parts
+                .next()
+                .ok_or_else(on_error)?
+                .parse()
+                .map_err(|_| on_error())?,
+            v_parts
+                .next()
+                .ok_or_else(on_error)?
+                .parse()
+                .map_err(|_| on_error())?,
+            v_parts
+                .next()
+                .ok_or_else(on_error)?
+                .parse()
+                .map_err(|_| on_error())?,
+        ])?;
     }
 
-    const fn to_float(self) -> FloatCoordinate {
-        FloatCoordinate {
-            x: self.x as f64,
-            y: self.y as f64,
-            z: self.z as f64,
-        }
+    if input.is_part_one() {
+        Ok(part1(i.slice()) as u64)
+    } else {
+        Ok(part2(i.slice()) as u64)
     }
 }
 
-#[derive(Copy, Clone, Default)]
-struct Particle {
-    position: Coordinate,
-    velocity: Coordinate,
+pub fn part1(input: &[[i64; 6]]) -> u32 {
+    let mut result = 0;
+
+    for (index, &[a, b, _, c, d, _]) in input[1..].iter().enumerate() {
+        for &[e, f, _, g, h, _] in &input[..index + 1] {
+            // If the determinant is zero there is no solution possible
+            // which implies the trajectories are parallel.
+            let determinant = d * g - c * h;
+            if determinant == 0 {
+                continue;
+            }
+
+            // Invert the 2x2 matrix then multiply by the respective columns to find the times.
+            let t = (g * (f - b) - h * (e - a)) / determinant;
+            let u = (c * (f - b) - d * (e - a)) / determinant;
+
+            // We can pick either the first or second hailstone to find the intersection position.
+            let x = a + t * c;
+            let y = b + t * d;
+
+            // Both times must be in the future and the position within the specified area.
+            if t >= 0 && u >= 0 && RANGE.contains(&x) && RANGE.contains(&y) {
+                result += 1;
+            }
+        }
+    }
+
+    result
 }
 
-impl Particle {
-    fn intersects_xy_with(&self, other: Self, interval: &RangeInclusive<i128>) -> bool {
-        // position_1.x + t0 * speed_1.x = position_2.x + t1 * speed_2.x
-        // position_1.y + t0 * speed_1.y = position_2.y + t1 * speed_2.y
-        // =>
-        // t0 * speed_1.x - t1 * speed_2.x = (position_2.x - position_1.x)
-        // t0 * speed_1.y - t1 * speed_2.y = (position_2.y - position_2.y)
-        // =>
-        // [ speed_1.x -speed2.x ] x [ t0 ] = [ position_2.x-position_1.x ]
-        // [ speed_1.x -speed2.y ]   [ t1 ]   [ position_2.y-position_2.y ]
-        // =>
-        // AX = B where X is unknown
-        // =>
-        // X = inv(A) * B
-        //
-        // Inverse of 2x2 matrix:
-        // For A = [ a b ]
-        //         [ c d ]
-        // inv(A) = 1 / (ad - bc) * [  d -b ]
-        //                          [ -c  a ]
-        let a = self.velocity.x;
-        let b = -other.velocity.x;
-        let c = self.velocity.y;
-        let d = -other.velocity.y;
-        let b_1 = other.position.x - self.position.x;
-        let b_2 = other.position.y - self.position.y;
-        let div = a * d - b * c;
-        if div == 0 {
-            return false;
-        }
-        let t_1 = (d * b_1 - b * b_2) as f64 / div as f64;
-        let t_2 = (-c * b_1 + a * b_2) as f64 / div as f64;
-        if t_1 < 0. || t_2 < 0. {
-            return false;
-        }
-        let x = t_1.mul_add(self.velocity.x as f64, self.position.x as f64);
-        let y = t_1.mul_add(self.velocity.y as f64, self.position.y as f64);
-        interval.contains(&(x as i128)) && interval.contains(&(y as i128))
-    }
+pub fn part2(input: &[[i64; 6]]) -> i128 {
+    // Calculations need the range of `i128`.
+    let widen = |i: usize| {
+        let [px, py, pz, vx, vy, vz] = input[i].map(|n| n as i128);
+        let p = Vector {
+            x: px,
+            y: py,
+            z: pz,
+        };
+        let v = Vector {
+            x: vx,
+            y: vy,
+            z: vz,
+        };
+        (p, v)
+    };
 
-    const fn to_float(self) -> FloatParticle {
-        FloatParticle {
-            p: self.position.to_float(),
-            v: self.velocity.to_float(),
-        }
-    }
+    // Take 3 arbitrary hailstones.
+    let (p0, v0) = widen(0);
+    let (p1, v1) = widen(1);
+    let (p2, v2) = widen(2);
+
+    // Subtract the positions and velocities to make them relative.
+    // The first hailstone is stationary at the origin.
+    let p3 = p1.sub(p0);
+    let p4 = p2.sub(p0);
+    let v3 = v1.sub(v0);
+    let v4 = v2.sub(v0);
+
+    // Find the normal to the plane that the second and third hailstones velocity lies in.
+    // This is the cross product of their respective position and velocity.
+    // The cross product `s` of these two vectors is the same direction but not necessarily the
+    // same magnitude of the desired velocity of the rock.
+    // Only the direction is relevant (not the magnitude) so we can normalize the vector by the
+    // GCD of its components in order to prevent numeric overflow.
+    let q = v3.cross(p3).gcd();
+    let r = v4.cross(p4).gcd();
+    let s = q.cross(r).gcd();
+
+    // Find the times when the second and third hailstone intercept this vector.
+    // If the times are different then we can extrapolate the original position of the rock.
+    let t = (p3.y * s.x - p3.x * s.y) / (v3.x * s.y - v3.y * s.x);
+    let u = (p4.y * s.x - p4.x * s.y) / (v4.x * s.y - v4.y * s.x);
+    assert!(t != u);
+
+    // Calculate the original position of the rock, remembering to add the first hailstone's
+    // position to convert back to absolute coordinates.
+    let a = p0.add(p3).sum();
+    let b = p0.add(p4).sum();
+    let c = v3.sub(v4).sum();
+    (u * a - t * b + u * t * c) / (u - t)
 }
 
 #[test]
--- crates/core/src/year2023/day25.rs
+++ crates/core/src/year2023/day25.rs
@@ -1,92 +1,178 @@
-use crate::common::array_deque::ArrayDeque;
-use crate::common::id_assigner_copy::IdAssigner;
-use crate::input::{on_error, Input};
+use crate::input::Input;
 
-const MAX_ENTRIES: usize = 2000;
-const MAX_CONNECTIONS: usize = 10;
-const INITIAL_ELEMENT_ID: u16 = 0;
-const EMPTY: u16 = u16::MAX;
+use std::collections::VecDeque;
 
+/// Store the graph as an [adjacency list](https://en.wikipedia.org/wiki/Adjacency_list).
+/// Each node has a unique index in the `nodes` vec.
+/// Each directed edge has a unique index in the `edges` vec.
+pub struct InputStruct {
+    edges: Vec<usize>,
+    nodes: Vec<(usize, usize)>,
+}
+
+impl InputStruct {
+    /// Convenience function to return an iterator of `(edge, node)` pairs.
+    #[inline]
+    fn neighbours(&self, node: usize) -> impl Iterator<Item = (usize, usize)> + '_ {
+        let (start, end) = self.nodes[node];
+        (start..end).map(|edge| (edge, self.edges[edge]))
+    }
+}
+
+/// Solution from https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2023/day25.rs
+/// Convert the input to use numeric indices instead of string keys for speed.
+/// Each node is assigned a unique index on a first come first served basis.
+/// Then the edges are gathered into a single vec so that each edge also has a unique index.
+///
+/// As both node and edge indices are contigous this allows us to use a vec to store previously
+/// seen values which is must faster than using a `HashMap`.
 pub fn solve(input: &Input) -> Result<u64, String> {
-    let mut id_assigner = IdAssigner::<MAX_ENTRIES, &str>::new("");
-    let mut entries = [Entry::default(); MAX_ENTRIES];
+    let mut lookup = [usize::MAX; 26 * 26 * 26];
+    let mut neighbours = Vec::with_capacity(2_000);
 
-    for line in input.text.lines() {
-        let (from, to) = line.split_once(": ").ok_or_else(on_error)?;
-        let from = id_assigner.id_of(from)?;
-        for to in to.split(' ') {
-            let to = id_assigner.id_of(to)?;
-            for (from, to) in [(from, to), (to, from)] {
-                let entry = &mut entries[from as usize];
-                entry.to[entry.num_entries as usize] = to;
-                entry.num_entries += 1;
+    for line in input.text.lines().map(str::as_bytes) {
+        let first = perfect_minimal_hash(&mut lookup, &mut neighbours, line);
+
+        // The graph is undirected so each link is bidirectional.
+        for chunk in line[5..].chunks(4) {
+            let second = perfect_minimal_hash(&mut lookup, &mut neighbours, chunk);
+            neighbours[first].push(second);
+            neighbours[second].push(first);
+        }
+    }
+
+    // Assign each edge a unique index. Each node then specifies a range into the edges vec.
+    let mut edges = Vec::with_capacity(5_000);
+    let mut nodes = Vec::with_capacity(neighbours.len());
+
+    for list in neighbours {
+        let start = edges.len();
+        let end = edges.len() + list.len();
+        edges.extend(list);
+        nodes.push((start, end));
+    }
+
+    let input_struct = InputStruct { edges, nodes };
+
+    // Arbitrarily pick the first node then find the furthest node from it.
+    let start = furthest(&input_struct, 0);
+    // Find the furthest node from start. The graph is constructed so that the minimum cut is
+    // in the center of the graph, so start and end will be on opposite sides of the cut.
+    let end = furthest(&input_struct, start);
+    // Find the size of the graph still connected to start after the cut.
+    let size = flow(&input_struct, start, end);
+    Ok((size * (input_struct.nodes.len() - size)) as u64)
+}
+
+/// Each node's name is exactly 3 lowercase ASCII letters. First we calculate a
+/// [perfect hash](https://en.wikipedia.org/wiki/Perfect_hash_function) by converting to a base 26
+/// number. Then we construct a perfect *minimal* hash by using the first index to lookup a
+/// contigous index into the nodes vec.
+fn perfect_minimal_hash(lookup: &mut [usize], nodes: &mut Vec<Vec<usize>>, slice: &[u8]) -> usize {
+    // Base 26 index.
+    let hash = slice[..3]
+        .iter()
+        .fold(0, |acc, b| 26 * acc + ((b - b'a') as usize));
+    let mut index = lookup[hash];
+
+    // First time seeing this key so push a new node and return its index.
+    if index == usize::MAX {
+        index = nodes.len();
+        lookup[hash] = index;
+        nodes.push(Vec::with_capacity(10));
+    }
+
+    index
+}
+
+/// BFS across the graph to find the furthest nodes from start.
+fn furthest(input: &InputStruct, start: usize) -> usize {
+    let mut todo = VecDeque::new();
+    todo.push_back(start);
+
+    // The node indices are also their key so we can use a vec instead of a HashSet for speed.
+    let mut seen = vec![false; input.nodes.len()];
+    seen[start] = true;
+
+    let mut result = start;
+
+    while let Some(current) = todo.pop_front() {
+        // The last node visited will be the furthest.
+        result = current;
+
+        for (_, next) in input.neighbours(current) {
+            if !seen[next] {
+                todo.push_back(next);
+                seen[next] = true;
             }
         }
     }
 
-    let mut furthest_away = None;
+    result
+}
 
-    for _ in 0..3 {
-        let (mut path_end, previous) = bfs(&entries, furthest_away)?;
-        furthest_away = Some(path_end);
+/// Simplified approach based on Edmonds–Karp algorithm.
+fn flow(input: &InputStruct, start: usize, end: usize) -> usize {
+    let mut todo = VecDeque::new();
+    // The path forms a linked list. During the BFS each path shares most nodes, so it's
+    // more efficient both in space and speed to store the path as a linked list instead
+    // of multiple copies of `vec`s.
+    let mut path = Vec::new();
+    // The capacity of each edge is 1 so only allow each edge to be used once.
+    let mut used = vec![false; input.edges.len()];
+    // The number of nodes from the 4th BFS is the size of one part of the cut graph.
+    let mut result = 0;
 
-        while path_end != 0 {
-            let from = previous[path_end as usize];
-            let entry = &mut entries[from as usize];
-            for i in 0..entry.num_entries {
-                if entry.to[i as usize] == path_end {
-                    // Delete connection.
-                    entry.to.swap(i as usize, entry.num_entries as usize);
-                    entry.num_entries -= 1;
-                    break;
+    // We know the minimum cut is 3, so the 4th iteration will only be able to reach nodes
+    // on start's side.
+    for _ in 0..4 {
+        todo.push_back((start, usize::MAX));
+        result = 0;
+
+        let mut seen = vec![false; input.nodes.len()];
+        seen[start] = true;
+
+        while let Some((current, head)) = todo.pop_front() {
+            // Count how many nodes we visit.
+            result += 1;
+
+            // If we reached the end then add each edge of the path to `used`
+            // so that it can be used only once.
+            if current == end {
+                let mut index = head;
+
+                // Traverse the linked list.
+                while index != usize::MAX {
+                    let (edge, next) = path[index];
+                    used[edge] = true;
+                    index = next;
+                }
+
+                break;
+            }
+
+            // Find neighbouring nodes to explore, only allowing each edge to be used once.
+            for (edge, next) in input.neighbours(current) {
+                if !used[edge] && !seen[next] {
+                    seen[next] = true;
+                    todo.push_back((next, path.len()));
+                    path.push((edge, head));
                 }
             }
-            path_end = from;
         }
+
+        // Re-use for each iteration as a minor optimization.
+        todo.clear();
+        path.clear();
     }
 
-    let (_, previous) = bfs(&entries, furthest_away)?;
-    let one_group_size = previous.iter().filter(|&&p| p != EMPTY).count() as u64;
-    let other_group_size = id_assigner.len() as u64 - one_group_size;
-    Ok(one_group_size * other_group_size)
-}
-
-fn bfs(
-    entries: &[Entry; MAX_ENTRIES],
-    target: Option<u16>,
-) -> Result<(u16, [u16; MAX_ENTRIES]), String> {
-    let mut previous = [EMPTY; MAX_ENTRIES];
-    let mut work_queue = ArrayDeque::<2000, u16>::new();
-    work_queue.push_back(INITIAL_ELEMENT_ID)?;
-
-    let mut last = 0;
-    while let Some(current) = work_queue.pop_front() {
-        last = current;
-        if target.is_some_and(|t| t == current) {
-            break;
-        }
-        let num_entries = entries[current as usize].num_entries;
-        for &to in &entries[current as usize].to[0..(num_entries as usize)] {
-            if previous[to as usize] == EMPTY {
-                previous[to as usize] = current;
-                work_queue.push_back(to)?;
-            }
-        }
-    }
-
-    Ok((last, previous))
-}
-
-#[derive(Copy, Clone, Default)]
-struct Entry {
-    to: [u16; MAX_CONNECTIONS],
-    num_entries: u8,
+    result
 }
 
 #[test]
 pub fn tests() {
-    use crate::input::test_part_one_no_allocations;
+    use crate::input::test_part_one;
 
     let real_input = include_str!("day25_input.txt");
-    test_part_one_no_allocations!(real_input => 543_564);
+    test_part_one!(real_input => 543_564);
 }
