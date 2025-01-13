a55f522a8a113a794e45642b4eda410a8d1aef8b
Fredrik Fornwall <fredrik@fornwall.net>
2025-01-13 10:30:56 +0100

    2024-23 fix

--- crates/core/src/year2024/day23.rs
+++ crates/core/src/year2024/day23.rs
@@ -1,64 +1,112 @@
+use std::collections::BTreeSet;
 use std::collections::{HashMap, HashSet};
 
-use crate::{
-    common::{array_stack::ArrayStack, id_assigner::IdAssigner},
-    input::{on_error, Input},
-    year2017::disjoint_set::DisjointSet,
-};
+use crate::common::array_stack::ArrayStack;
+use crate::input::{on_error, Input};
 
-pub fn solve(input: &Input) -> Result<u32, String> {
-    let mut with_t = ArrayStack::<1000, u16>::new();
-    let mut to_adjacency = HashMap::new();
+pub fn solve(input: &Input) -> Result<String, String> {
+    let mut nodes = HashMap::<u16, Vec<u16>>::with_capacity(1_000);
+    let mut edges = vec![[false; 676]; 676];
 
-    for line in input.text.lines() {
-        let (s1, s2) = line.split_once('-').ok_or_else(on_error)?;
-        let (n1, n2) = (to_num(s1), to_num(s2));
+    for edge in input.text.as_bytes().chunks(6) {
+        let from = to_num(&edge[..2]);
+        let to = to_num(&edge[3..]);
 
-        to_adjacency.entry(n1).or_insert_with(Vec::new).push(n2);
-        to_adjacency.entry(n2).or_insert_with(Vec::new).push(n1);
+        nodes.entry(from).or_default().push(to);
+        nodes.entry(to).or_default().push(from);
 
-        if s1.starts_with('t') {
-            with_t.push(n1)?;
-        }
-        if s2.starts_with('t') {
-            with_t.push(n2)?;
-        }
+        edges[from as usize][to as usize] = true;
+        edges[to as usize][from as usize] = true;
     }
 
-    let mut already_considered = HashSet::new();
-    let mut result = 0;
-    let slice = with_t.slice_mut();
-    slice.sort_unstable();
-    let mut last = u16::MAX;
-    for n in slice {
-        if *n != last {
-            let adjacency_list = to_adjacency.get(n).unwrap();
-            for (i, x) in adjacency_list.iter().enumerate() {
-                for (j, y) in adjacency_list.iter().enumerate().skip(i + 1) {
-                    if to_adjacency.get(x).unwrap().iter().any(|e| e == y) {
-                        let mut aa = [*n, *x, *y];
-                        aa.sort_unstable();
-                        if already_considered.insert(aa) {
-                            result += 1;
+    let mut seen = [false; 1024];
+
+    Ok(if input.is_part_one() {
+        let mut triangles = 0;
+
+        for &n1 in nodes.keys() {
+            seen[n1 as usize] = true;
+
+            if let Some(neighbours) = nodes.get(&n1) {
+                for (i, &n2) in neighbours.iter().enumerate() {
+                    for &n3 in neighbours.iter().skip(i) {
+                        if !seen[n2 as usize]
+                            && !seen[n3 as usize]
+                            && edges[n2 as usize][n3 as usize]
+                            && (starts_with_t(n1) || starts_with_t(n2) || starts_with_t(n3))
+                        {
+                            triangles += 1;
                         }
                     }
                 }
             }
-            last = *n;
         }
-    }
 
-    Ok(result)
+        triangles.to_string()
+    } else {
+        let mut clique = ArrayStack::<1024, u16>::new();
+        let mut largest = [0_u16; 1024];
+        let mut largest_len = 0;
+
+        for (n1, neighbours) in nodes {
+            if !seen[n1 as usize] {
+                clique.clear();
+                clique.push(n1)?;
+
+                for n2 in neighbours {
+                    if clique
+                        .slice()
+                        .iter()
+                        .all(|&c| edges[n2 as usize][c as usize])
+                    {
+                        seen[n2 as usize] = true;
+                        clique.push(n2)?;
+                    }
+                }
+
+                if clique.len() > largest_len {
+                    largest_len = clique.len();
+                    largest[0..largest_len].copy_from_slice(clique.slice());
+                }
+            }
+        }
+
+        let mut result = String::new();
+        largest[0..largest_len].sort_unstable();
+        let mut first = true;
+        for &n in &largest[0..largest_len] {
+            if first {
+                first = false;
+            } else {
+                result.push(',');
+            }
+            result.push_str(&from_num(n));
+        }
+        result
+    })
 }
 
-fn to_num(p: &str) -> u16 {
-    let p = p.as_bytes();
-    u16::from(p[0] - b'a') + 29 * u16::from(p[1] - b'a')
+fn to_num(p: &[u8]) -> u16 {
+    26 * u16::from(p[0] - b'a') + u16::from(p[1] - b'a')
+}
+
+fn from_num(num: u16) -> String {
+    let first_char = ((num / 26) as u8 + b'a') as char;
+    let second_char = ((num % 26) as u8 + b'a') as char;
+    format!("{first_char}{second_char}")
+}
+
+fn starts_with_t(num: u16) -> bool {
+    let first_char = (num / 26) as u8 + b'a';
+    first_char == b't'
 }
 
 #[test]
 pub fn tests() {
-    use crate::input::{test_part_one_no_allocations, test_part_two_no_allocations};
+    use crate::input::{test_part_one, test_part_two};
+
+    assert_eq!("ab", from_num(to_num(b"ab")));
+    assert_eq!("yz", from_num(to_num(b"yz")));
 
     let test_input = "kh-tc
 qp-kh
@@ -92,10 +140,10 @@ co-tc
 wh-qp
 tb-vc
 td-yn";
-    test_part_one_no_allocations!(test_input => 7);
-    //test_part_two_no_allocations!(test_input => 0);
+    test_part_one!(test_input => "7".to_string());
+    test_part_two!(test_input => "co,de,ka,ta".to_string());
 
     let real_input = include_str!("day23_input.txt");
-    test_part_one_no_allocations!(real_input => 1175);
-    //test_part_two_no_allocations!(real_input => 0);
+    test_part_one!(real_input => "1175".to_string());
+    test_part_two!(real_input => "bw,dr,du,ha,mm,ov,pj,qh,tz,uv,vq,wq,xw".to_string());
 }
