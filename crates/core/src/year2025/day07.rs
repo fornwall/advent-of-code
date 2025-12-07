8a24fdcad1a9f3fca1e8e8952d524647ad693072
Fredrik Fornwall <fredrik.fornwall@avanza.se>
2025-12-07 09:58:44 +0100

    2025-07

--- crates/core/src/year2025/day07.rs
+++ crates/core/src/year2025/day07.rs
@@ -1,19 +1,65 @@
-use crate::input::Input;
+use crate::input::{Input, on_error};
 
-pub const fn solve(_input: &Input) -> Result<u64, String> {
-    Ok(0)
+pub fn solve(input: &Input) -> Result<u64, String> {
+    const MAX_WIDTH: usize = 150;
+    let grid_width = input.text.lines().next().ok_or_else(on_error)?.len();
+    if grid_width > MAX_WIDTH {
+        return Err("Input too large".to_string());
+    }
+
+    let mut num_timelines_at_x = [0_u64; MAX_WIDTH];
+    let beam_start_x = input
+        .text
+        .bytes()
+        .position(|b| b == b'S')
+        .ok_or_else(on_error)?;
+    num_timelines_at_x[beam_start_x] = 1;
+
+    let mut num_splits = 0;
+    for line in input.text.lines().skip(1) {
+        for (x, char) in line.bytes().enumerate() {
+            let is_split_point = char == b'^';
+            let num_timelines_here = num_timelines_at_x[x];
+            if num_timelines_here > 0 && is_split_point {
+                num_splits += 1;
+                num_timelines_at_x[x - 1] += num_timelines_here;
+                num_timelines_at_x[x] = 0;
+                num_timelines_at_x[x + 1] += num_timelines_here;
+            }
+        }
+    }
+
+    Ok(if input.is_part_one() {
+        num_splits
+    } else {
+        num_timelines_at_x.iter().sum()
+    })
 }
 
 #[test]
-#[ignore]
 pub fn tests() {
     use crate::input::{test_part_one_no_allocations, test_part_two_no_allocations};
 
-    let test_input = "...";
-    test_part_one_no_allocations!(test_input => 0);
-    test_part_two_no_allocations!(test_input => 0);
+    let test_input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+    test_part_one_no_allocations!(test_input => 21);
+    test_part_two_no_allocations!(test_input => 40);
 
     let real_input = include_str!("day07_input.txt");
-    test_part_one_no_allocations!(real_input => 0);
-    test_part_two_no_allocations!(real_input => 0);
+    test_part_one_no_allocations!(real_input => 1638);
+    test_part_two_no_allocations!(real_input => 7_759_107_121_385);
 }
